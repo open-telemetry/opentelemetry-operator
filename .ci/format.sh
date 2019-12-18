feb79cdd6d9365cd39daa4d4edd2a25cfd3cf4cd
Juraci Paixão Kröhling <juraci@kroehling.de>
2019-12-18 13:08:30 +0100

    Moved 'format' to external script (#4)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

---
+++ .ci/format.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+GOFMT=goimports
+
+command -v ${GOFMT} > /dev/null
+if [ $? != 0 ]; then
+    if [ -z ${GOPATH} ]; then
+        GOFMT="${GOPATH}/bin/goimports"
+    fi
+fi
+
+${GOFMT} -local "github.com/open-telemetry/opentelemetry-operator" -l -w $(git ls-files "*\.go" | grep -v vendor)
