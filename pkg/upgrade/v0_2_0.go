d2e3a9a8a635fd25d68c91f7c2270f2636bae48f
Juraci Paixão Kröhling <juraci@kroehling.de>
2019-12-20 15:04:26 +0100

    Updated OpenTelemetry Collector to 0.2.0 (#8)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- pkg/upgrade/v0_2_0.go
+++ pkg/upgrade/v0_2_0.go
@@ -5,7 +5,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-operator/pkg/client"
 )
 
-func upgrade0_0_2(cl *client.Clientset, otelcol *v1alpha1.OpenTelemetryCollector) (*v1alpha1.OpenTelemetryCollector, error) {
+func upgrade0_2_0(cl *client.Clientset, otelcol *v1alpha1.OpenTelemetryCollector) (*v1alpha1.OpenTelemetryCollector, error) {
 	// this has the same content as `noop`, but it's added a separate function
 	// to serve as template for versions with an actual upgrade procedure
 	return otelcol, nil
