844ae6e8260db60d68e0f7bb4ae50dfb8c21df9e
Juraci Paixão Kröhling <juraci@kroehling.de>
2020-12-04 13:27:48 +0100

    Bumped OpenTelemetry Collector to v0.16.0 (#135)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- pkg/collector/parser/receiver_otlp.go
+++ pkg/collector/parser/receiver_otlp.go
@@ -1,18 +1,106 @@
 package parser
 
-import "github.com/go-logr/logr"
+import (
+	"fmt"
 
-const parserNameOTLP = "__otlp"
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+var _ ReceiverParser = &OTLPReceiverParser{}
+
+const (
+	parserNameOTLP = "__otlp"
+
+	defaultOTLPGRPCPort       int32 = 4317
+	defaultOTLPGRPCLegacyPort int32 = 55680
+	defaultOTLPHTTPPort       int32 = 55681
+)
+
+// OTLPReceiverParser parses the configuration for OTLP receivers
+type OTLPReceiverParser struct {
+	logger logr.Logger
+	name   string
+	config map[interface{}]interface{}
+}
 
 // NewOTLPReceiverParser builds a new parser for OTLP receivers
 func NewOTLPReceiverParser(logger logr.Logger, name string, config map[interface{}]interface{}) ReceiverParser {
-	return &GenericReceiver{
-		logger:      logger,
-		name:        name,
-		config:      config,
-		defaultPort: 55680,
-		parserName:  parserNameOTLP,
+	if protocols, ok := config["protocols"].(map[interface{}]interface{}); ok {
+		return &OTLPReceiverParser{
+			logger: logger,
+			name:   name,
+			config: protocols,
+		}
 	}
+
+	return &OTLPReceiverParser{
+		name:   name,
+		config: map[interface{}]interface{}{},
+	}
+}
+
+// Ports returns all the service ports for all protocols in this parser
+func (o *OTLPReceiverParser) Ports() ([]corev1.ServicePort, error) {
+	ports := []corev1.ServicePort{}
+
+	for _, protocol := range []struct {
+		name         string
+		defaultPorts []corev1.ServicePort
+	}{
+		{
+			name: "grpc",
+			defaultPorts: []corev1.ServicePort{
+				{
+					Name:       portName(fmt.Sprintf("%s-grpc", o.name), defaultOTLPGRPCPort),
+					Port:       defaultOTLPGRPCPort,
+					TargetPort: intstr.FromInt(int(defaultOTLPGRPCPort)),
+				},
+				{
+					Name:       portName(fmt.Sprintf("%s-grpc-legacy", o.name), defaultOTLPGRPCLegacyPort),
+					Port:       defaultOTLPGRPCLegacyPort,
+					TargetPort: intstr.FromInt(int(defaultOTLPGRPCPort)), // we target the official port, not the legacy
+				},
+			},
+		},
+		{
+			name: "http",
+			defaultPorts: []corev1.ServicePort{{
+				Name:       portName(fmt.Sprintf("%s-http", o.name), defaultOTLPHTTPPort),
+				Port:       defaultOTLPHTTPPort,
+				TargetPort: intstr.FromInt(int(defaultOTLPHTTPPort)),
+			}},
+		},
+	} {
+		// do we have the protocol specified at all?
+		if receiverProtocol, ok := o.config[protocol.name]; ok {
+			// we have the specified protocol, we definitely need a service port
+			nameWithProtocol := fmt.Sprintf("%s-%s", o.name, protocol.name)
+			var protocolPort *corev1.ServicePort
+
+			// do we have a configuration block for the protocol?
+			settings, ok := receiverProtocol.(map[interface{}]interface{})
+			if ok {
+				protocolPort = singlePortFromConfigEndpoint(o.logger, nameWithProtocol, settings)
+			}
+
+			// have we parsed a port based on the configuration block?
+			// if not, we use the default port
+			if protocolPort == nil {
+				ports = append(ports, protocol.defaultPorts...)
+			} else {
+				ports = append(ports, *protocolPort)
+			}
+		}
+	}
+
+	return ports, nil
+}
+
+// ParserName returns the name of this parser
+func (o *OTLPReceiverParser) ParserName() string {
+	return parserNameOTLP
 }
 
 func init() {
