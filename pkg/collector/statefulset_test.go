4262d99611b5b18fe2e93df4deece0fe952c38ab
Huy Vo <hvoam@amazon.com>
2021-04-30 14:15:15 -0700

    Add unit tests for StatefulSet

---
+++ pkg/collector/statefulset_test.go
@@ -0,0 +1,82 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/open-telemetry/opentelemetry-operator/api/v1alpha1"
+	"github.com/open-telemetry/opentelemetry-operator/internal/config"
+	. "github.com/open-telemetry/opentelemetry-operator/pkg/collector"
+)
+
+func TestStatefulSetNewDefault(t *testing.T) {
+	// prepare
+	otelcol := v1alpha1.OpenTelemetryCollector{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "my-instance",
+		},
+	}
+	cfg := config.New()
+
+	// test
+	ss := StatefulSet(cfg, logger, otelcol)
+
+	// verify
+	assert.Equal(t, "my-instance-collector", ss.Name)
+	assert.Equal(t, "my-instance-collector", ss.Labels["app.kubernetes.io/name"])
+	assert.Equal(t, "true", ss.Annotations["prometheus.io/scrape"])
+	assert.Equal(t, "8888", ss.Annotations["prometheus.io/port"])
+	assert.Equal(t, "/metrics", ss.Annotations["prometheus.io/path"])
+
+	assert.Len(t, ss.Spec.Template.Spec.Containers, 1)
+
+	// none of the default annotations should propagate down to the pod
+	assert.Empty(t, ss.Spec.Template.Annotations)
+
+	// the pod selector should match the pod spec's labels
+	assert.Equal(t, ss.Spec.Selector.MatchLabels, ss.Spec.Template.Labels)
+
+	// assert correct service name
+	assert.Equal(t, "my-instance-collector", ss.Spec.ServiceName)
+
+	// assert correct pod management policy
+	assert.Equal(t, v1.PodManagementPolicyType("Parallel"), ss.Spec.PodManagementPolicy)
+}
+
+func TestStatefulSetReplicas(t *testing.T) {
+	// prepare
+	var replicaInt int32
+	replicaInt = 3
+	otelcol := v1alpha1.OpenTelemetryCollector{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "my-instance",
+		},
+		Spec: v1alpha1.OpenTelemetryCollectorSpec{
+			Replicas: &replicaInt,
+		},
+	}
+	cfg := config.New()
+
+	// test
+	ss := StatefulSet(cfg, logger, otelcol)
+
+	// assert correct number of replicas
+	assert.Equal(t, int32(3), *ss.Spec.Replicas)
+}
