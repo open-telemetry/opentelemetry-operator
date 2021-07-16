99618dbd4e47109fa8fac89bbb25e1e47c4127b4
Vineeth Pothulapati <vineethpothulapati@outlook.com>
2021-07-16 19:24:31 +0530

    Bump kubectl-kuttl to v0.11.0 (#343)

--- hack/install-kuttl.sh
+++ hack/install-kuttl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo curl -Lo /usr/local/bin/kubectl-kuttl https://github.com/kudobuilder/kuttl/releases/download/v0.7.2/kubectl-kuttl_0.7.2_linux_x86_64
+sudo curl -Lo /usr/local/bin/kubectl-kuttl https://github.com/kudobuilder/kuttl/releases/download/v0.11.0/kubectl-kuttl_0.11.0_linux_x86_64
 sudo chmod +x /usr/local/bin/kubectl-kuttl
 export PATH=$PATH:/usr/local/bin
