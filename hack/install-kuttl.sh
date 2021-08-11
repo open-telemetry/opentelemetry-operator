2d03c272761db3e327ea3abda34fef49fff4a049
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-08-11 11:34:45 +0200

    Bump kuttl to v0.11.1 (#392)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- hack/install-kuttl.sh
+++ hack/install-kuttl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo curl -Lo /usr/local/bin/kubectl-kuttl https://github.com/kudobuilder/kuttl/releases/download/v0.11.0/kubectl-kuttl_0.11.0_linux_x86_64
+sudo curl -Lo /usr/local/bin/kubectl-kuttl https://github.com/kudobuilder/kuttl/releases/download/v0.11.1/kubectl-kuttl_0.11.1_linux_x86_64
 sudo chmod +x /usr/local/bin/kubectl-kuttl
 export PATH=$PATH:/usr/local/bin
