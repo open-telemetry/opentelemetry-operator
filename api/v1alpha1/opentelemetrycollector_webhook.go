5a68d3990082d39e9674e6b52bc66ff831cbb486
Rahul Varma <rahvarm@amazon.com>
2021-08-11 18:09:21 -0700

    Squashed commit of the following:
    commit a4968257e812ddba69068a29b965674c8c455c48
    Author: Rahul Varma <rahvarm@amazon.com>
    Date:   Wed Aug 11 14:53:36 2021 -0400
    
        Update api/v1alpha1/opentelemetrycollector_webhook.go
    
        Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>
    
    commit 3d37148696742ed1a2cda7c23357834b1bc803f5
    Author: Rahul Varma <rahvarm@amazon.com>
    Date:   Wed Aug 11 10:34:44 2021 -0700
    
        Added validation in webhook to validate prometheus config
    
    commit 33cb04253bb223613f4a26e2b36ce52a960a912f
    Author: Rahul Varma <rahvarm@amazon.com>
    Date:   Wed Aug 11 07:16:45 2021 -0700
    
        Updated docker image to quay.io image
    
    commit a7764b97a208aa268864f298dd81268584407344
    Author: Rahul Varma <rahvarm@amazon.com>
    Date:   Tue Aug 10 20:20:32 2021 -0700
    
        Added version management & Minor fixes

--- api/v1alpha1/opentelemetrycollector_webhook.go
+++ api/v1alpha1/opentelemetrycollector_webhook.go
@@ -21,6 +21,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	ta "github.com/open-telemetry/opentelemetry-operator/pkg/targetallocator/adapters"
 )
 
 // log is for logging in this package.
@@ -94,6 +96,14 @@ func (r *OpenTelemetryCollector) validateCRDSpec() error {
 	// validate target allocation
 	if r.Spec.TargetAllocator.Enabled && r.Spec.Mode != ModeStatefulSet {
 		return fmt.Errorf("the OpenTelemetry Collector mode is set to %s, which does not support the target allocation deployment", r.Spec.Mode)
+	}
+
+	// validate Prometheus config for target allocation
+	if r.Spec.TargetAllocator.Enabled {
+		_, err := ta.ConfigToPromConfig(r.Spec.Config)
+		if err != nil {
+			return fmt.Errorf("the OpenTelemetry Spec Prometheus configuration is incorrect, %s", err)
+		}
 	}
 
 	return nil
