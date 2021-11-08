480a48973803af2b5f2bdd78b05839671a58ab43
Anuraag Agrawal <anuraaga@gmail.com>
2021-11-08 20:53:40 +0900

    Add docker image containing nodejs auto instrumentation (#508)

---
+++ autoinstrumentation/nodejs/src/autoinstrumentation.ts
@@ -0,0 +1,12 @@
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-otlp-grpc';
+
+import { NodeSDK } from '@opentelemetry/sdk-node';
+
+const sdk = new NodeSDK({
+    autoDetectResources: true,
+    instrumentations: [getNodeAutoInstrumentations()],
+    traceExporter: new OTLPTraceExporter(),
+});
+
+sdk.start();
