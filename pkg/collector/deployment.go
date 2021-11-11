90876121b64ec7e68cbd2c9db02425da975d0b11
Pavol Loffay <p.loffay@gmail.com>
2021-11-11 14:02:27 +0100

    Fix project structure to align with kubebuilder scaffolding (#533)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- pkg/collector/deployment.go
+++ pkg/collector/deployment.go
@@ -20,7 +20,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/open-telemetry/opentelemetry-operator/apis/collector/v1alpha1"
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	"github.com/open-telemetry/opentelemetry-operator/internal/config"
 	"github.com/open-telemetry/opentelemetry-operator/pkg/naming"
 )
