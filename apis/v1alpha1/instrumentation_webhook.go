e279775c522445429cf43e0fba179a9f1b9979a0
Pavol Loffay <p.loffay@gmail.com>
2021-11-16 12:05:41 +0100

    Add validating webhook for Instrumentation kind (#543)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- apis/v1alpha1/instrumentation_webhook.go
+++ apis/v1alpha1/instrumentation_webhook.go
@@ -15,6 +15,10 @@
 package v1alpha1
 
 import (
+	"fmt"
+	"strconv"
+
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -52,4 +56,44 @@ func (r *Instrumentation) Default() {
 			r.Spec.Java.Image = val
 		}
 	}
+}
+
+// +kubebuilder:webhook:verbs=create;update,path=/validate-opentelemetry-io-v1alpha1-instrumentation,mutating=false,failurePolicy=fail,groups=opentelemetry.io,resources=instrumentations,versions=v1alpha1,name=vinstrumentationcreateupdate.kb.io,sideEffects=none,admissionReviewVersions={v1,v1beta1}
+// +kubebuilder:webhook:verbs=delete,path=/validate-opentelemetry-io-v1alpha1-instrumentation,mutating=false,failurePolicy=ignore,groups=opentelemetry.io,resources=instrumentations,versions=v1alpha1,name=vinstrumentationdelete.kb.io,sideEffects=none,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.Validator = &Instrumentation{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type.
+func (in *Instrumentation) ValidateCreate() error {
+	opentelemetrycollectorlog.Info("validate create", "name", in.Name)
+	return in.validate()
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type.
+func (in *Instrumentation) ValidateUpdate(old runtime.Object) error {
+	opentelemetrycollectorlog.Info("validate update", "name", in.Name)
+	return in.validate()
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type.
+func (in *Instrumentation) ValidateDelete() error {
+	opentelemetrycollectorlog.Info("validate delete", "name", in.Name)
+	return nil
+}
+
+func (in *Instrumentation) validate() error {
+	switch in.Spec.Sampler.Type {
+	case TraceIDRatio, ParentBasedTraceIDRatio:
+		if in.Spec.Sampler.Argument != "" {
+			rate, err := strconv.ParseFloat(in.Spec.Sampler.Argument, 64)
+			if err != nil {
+				return fmt.Errorf("spec.sampler.argument is not a number: %s", in.Spec.Sampler.Argument)
+			}
+			if rate < 0 || rate > 1 {
+				return fmt.Errorf("spec.sampler.argument should be in rage [0..1]: %s", in.Spec.Sampler.Argument)
+			}
+		}
+	case AlwaysOn, AlwaysOff, JaegerRemote, ParentBasedAlwaysOn, ParentBasedAlwaysOff, XRaySampler:
+	}
+	return nil
 }
