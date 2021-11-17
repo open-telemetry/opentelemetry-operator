45627a7dad0bde53f5951445b94c2afaa510e46a
Pavol Loffay <p.loffay@gmail.com>
2021-11-17 14:44:44 +0100

    Remove unused operator-sdk annotations from kinds (#556)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- apis/v1alpha1/opentelemetrycollector_types.go
+++ apis/v1alpha1/opentelemetrycollector_types.go
@@ -23,72 +23,59 @@ import (
 type OpenTelemetryCollectorSpec struct {
 	// Config is the raw JSON to be used as the collector's configuration. Refer to the OpenTelemetry Collector documentation for details.
 	// +required
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Config string `json:"config,omitempty"`
 
 	// Args is the set of arguments to pass to the OpenTelemetry Collector binary
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Args map[string]string `json:"args,omitempty"`
 
 	// Replicas is the number of pod instances for the underlying OpenTelemetry Collector
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Replicas *int32 `json:"replicas,omitempty"`
 
 	// ImagePullPolicy indicates the pull policy to be used for retrieving the container image (Always, Never, IfNotPresent)
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	ImagePullPolicy v1.PullPolicy `json:"imagePullPolicy,omitempty"`
 
 	// Image indicates the container image to use for the OpenTelemetry Collector.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Image string `json:"image,omitempty"`
 
 	// TargetAllocator indicates a value which determines whether to spawn a target allocation resource or not.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	TargetAllocator OpenTelemetryTargetAllocatorSpec `json:"targetAllocator,omitempty"`
 
 	// Mode represents how the collector should be deployed (deployment, daemonset, statefulset or sidecar)
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Mode Mode `json:"mode,omitempty"`
 
 	// ServiceAccount indicates the name of an existing service account to use with this instance.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	ServiceAccount string `json:"serviceAccount,omitempty"`
 
 	// SecurityContext will be set as the container security context.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	SecurityContext *v1.SecurityContext `json:"securityContext,omitempty"`
 
 	PodSecurityContext *v1.PodSecurityContext `json:"podSecurityContext,omitempty"`
 
 	// HostNetwork indicates if the pod should run in the host networking namespace.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	HostNetwork bool `json:"hostNetwork,omitempty"`
 
 	// VolumeClaimTemplates will provide stable storage using PersistentVolumes. Only available when the mode=statefulset.
 	// +optional
 	// +listType=atomic
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	VolumeClaimTemplates []v1.PersistentVolumeClaim `json:"volumeClaimTemplates,omitempty"`
 
 	// VolumeMounts represents the mount points to use in the underlying collector deployment(s)
 	// +optional
 	// +listType=atomic
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	VolumeMounts []v1.VolumeMount `json:"volumeMounts,omitempty"`
 
 	// Volumes represents which volumes to use in the underlying collector deployment(s).
 	// +optional
 	// +listType=atomic
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Volumes []v1.Volume `json:"volumes,omitempty"`
 
 	// Ports allows a set of ports to be exposed by the underlying v1.Service. By default, the operator
@@ -96,36 +83,30 @@ type OpenTelemetryCollectorSpec struct {
 	// used to open aditional ports that can't be inferred by the operator, like for custom receivers.
 	// +optional
 	// +listType=atomic
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Ports []v1.ServicePort `json:"ports,omitempty"`
 
 	// ENV vars to set on the OpenTelemetry Collector's Pods. These can then in certain cases be
 	// consumed in the config file for the Collector.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Env []v1.EnvVar `json:"env,omitempty"`
 
 	// List of sources to populate environment variables on the OpenTelemetry Collector's Pods.
 	// These can then in certain cases be consumed in the config file for the Collector.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	EnvFrom []v1.EnvFromSource `json:"envFrom,omitempty"`
 
 	// Resources to set on the OpenTelemetry Collector pods.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Resources v1.ResourceRequirements `json:"resources,omitempty"`
 
 	// Toleration to schedule OpenTelemetry Collector pods.
 	// This is only relevant to daemonsets, statefulsets and deployments
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	Tolerations []v1.Toleration `json:"tolerations,omitempty"`
 
 	// PodAnnotations is the set of annotations that will be attached to
 	// Collector and Target Allocator pods.
 	// +optional
-	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=true
 	PodAnnotations map[string]string `json:"podAnnotations,omitempty"`
 }
 
