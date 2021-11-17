c71adc2f8e83fd60385e8c688a39c61f72932de0
Anuraag Agrawal <anuraaga@gmail.com>
2021-11-17 18:55:39 +0900

    Delete namespaces created in tests. (#528)
    * Delete namespaces created in tests.
    
    * Lint

--- pkg/instrumentation/podmutator_test.go
+++ pkg/instrumentation/podmutator_test.go
@@ -398,6 +398,9 @@ func TestMutatePod(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			err := k8sClient.Create(context.Background(), &test.ns)
 			require.NoError(t, err)
+			defer func() {
+				_ = k8sClient.Delete(context.Background(), &test.ns)
+			}()
 			err = k8sClient.Create(context.Background(), &test.inst)
 			require.NoError(t, err)
 
