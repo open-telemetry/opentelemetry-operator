d6ceca9ea3851204d1e0f26bd764fb0aec50cbfc
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-11-17 13:24:12 +0900

    Fix merge

--- pkg/instrumentation/python_test.go
+++ pkg/instrumentation/python_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 
-	"github.com/open-telemetry/opentelemetry-operator/apis/instrumentation/v1alpha1"
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 )
 
 func TestInjectPythonSDK(t *testing.T) {
