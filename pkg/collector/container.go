db11311de1390ef41f9c096b039c9875bdea9d3b
Adrian Kostrubiak <Adrian.Kostrubiak@tomtom.com>
2021-11-25 15:16:35 -0500

    fix golangci-lint/import order  issues

--- pkg/collector/container.go
+++ pkg/collector/container.go
@@ -20,10 +20,9 @@ import (
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 
-	"github.com/open-telemetry/opentelemetry-operator/pkg/collector/adapters"
-
 	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	"github.com/open-telemetry/opentelemetry-operator/internal/config"
+	"github.com/open-telemetry/opentelemetry-operator/pkg/collector/adapters"
 	"github.com/open-telemetry/opentelemetry-operator/pkg/naming"
 )
 
