36e9369907bce6db84efccbf690c589b3f5cb5ec
Adrian Kostrubiak <Adrian.Kostrubiak@tomtom.com>
2021-11-25 11:33:44 -0500

    fix golangci-lint  issues

--- pkg/collector/adapters/config_to_probe.go
+++ pkg/collector/adapters/config_to_probe.go
@@ -1,10 +1,25 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package adapters
 
 import (
 	"errors"
+	"strings"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"strings"
 )
 
 var (
@@ -32,7 +47,7 @@ const (
 	defaultHealthCheckPort = 13133
 )
 
-// ConfigToContainerProbe converts the incoming configuration object into a container probe or returns an error
+// ConfigToContainerProbe converts the incoming configuration object into a container probe or returns an error.
 func ConfigToContainerProbe(config map[interface{}]interface{}) (*corev1.Probe, error) {
 	serviceProperty, ok := config["service"]
 	if !ok {
--- pkg/collector/container.go
+++ pkg/collector/container.go
@@ -16,9 +16,11 @@ package collector
 
 import (
 	"fmt"
+
 	"github.com/go-logr/logr"
-	"github.com/open-telemetry/opentelemetry-operator/pkg/collector/adapters"
 	corev1 "k8s.io/api/core/v1"
+
+	"github.com/open-telemetry/opentelemetry-operator/pkg/collector/adapters"
 
 	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	"github.com/open-telemetry/opentelemetry-operator/internal/config"
