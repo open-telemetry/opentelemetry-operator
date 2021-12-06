02a6f7369243ff71bb187626c8d71a9329cf4388
Adrian Kostrubiak <Adrian.Kostrubiak@tomtom.com>
2021-12-06 06:21:49 -0500

    fix import ordering

--- main.go
+++ main.go
@@ -22,8 +22,6 @@ import (
 	"runtime"
 	"strings"
 
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-
 	"github.com/spf13/pflag"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
@@ -32,6 +30,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
