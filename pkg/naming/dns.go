70910ad8fe0158edebbb0c6b30210a92b63016fb
Mihai Matache <mihai.matache@sap.com>
2021-12-07 16:18:22 +0200

    updated Jaeger copyright

--- pkg/naming/dns.go
+++ pkg/naming/dns.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright The Jaeger Authors.
+// Additional copyrights:
+// Copyright The Jaeger Authors
 
 package naming
 
