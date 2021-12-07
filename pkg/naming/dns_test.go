6ad9a4f4bc2c492b91fb57930f17feab7c515a2e
Mihai Matache <mihai.matache@sap.com>
2021-12-07 12:25:32 +0200

    fixed linting issues

--- pkg/naming/dns_test.go
+++ pkg/naming/dns_test.go
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package naming
 
 import (
@@ -22,11 +36,12 @@ func TestDnsName(t *testing.T) {
 		{"foo-", "fooa"},
 		{"-foo", "afoo"},
 	}
+	rule, err := regexp.Compile(`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`)
+	assert.NoError(t, err)
 
 	for _, tt := range tests {
 		assert.Equal(t, tt.out, DNSName(tt.in))
-		matched, err := regexp.MatchString(`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`, tt.out)
-		assert.NoError(t, err)
+		matched := rule.Match([]byte(tt.out))
 		assert.True(t, matched, "%v is not a valid name", tt.out)
 	}
 }
