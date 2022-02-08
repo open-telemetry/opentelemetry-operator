f5c19f70554da141529dfe3d25eaace42c91fb9a
Pavol Loffay <p.loffay@gmail.com>
2022-02-08 11:07:57 +0100

    fix lint
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- pkg/collector/upgrade/upgrade_test.go
+++ pkg/collector/upgrade/upgrade_test.go
@@ -16,12 +16,13 @@ package upgrade_test
 
 import (
 	"context"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"testing"
 
 	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	"github.com/open-telemetry/opentelemetry-operator/internal/version"
