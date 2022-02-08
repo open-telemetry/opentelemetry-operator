cd5074e4cb61f48372ff064b2ef91d9843721c73
Yuri Sa <yurimsa@gmail.com>
2022-02-08 19:26:13 +0100

    Upgrade script to version 0.43.0

---
+++ pkg/collector/upgrade/v0_43_0_test.go
@@ -0,0 +1,48 @@
+package upgrade_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
+	"github.com/open-telemetry/opentelemetry-operator/internal/version"
+	"github.com/open-telemetry/opentelemetry-operator/pkg/collector/upgrade"
+)
+
+func TestRemoveMetricsArgs(t *testing.T) {
+	// prepare
+	nsn := types.NamespacedName{Name: "my-instance", Namespace: "default"}
+	existing := v1alpha1.OpenTelemetryCollector{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      nsn.Name,
+			Namespace: nsn.Namespace,
+			Labels: map[string]string{
+				"app.kubernetes.io/managed-by": "opentelemetry-operator",
+			},
+		},
+		Spec: v1alpha1.OpenTelemetryCollectorSpec{
+			Args: map[string]string{
+				// this would not happen in the real world, as it's either one or another, but we aren't going that far
+				"--metrics-addr": ":8988",
+				"--metrics-level": "detailed",
+			},
+		},
+	}
+	existing.Status.Version = "0.42.0"
+
+	require.Contains(t, existing.Spec.Args, "--metrics-addr")
+	require.Contains(t, existing.Spec.Args, "--metrics-level")
+
+	// test
+	res, err := upgrade.ManagedInstance(context.Background(), logger, version.Get(), nil, existing)
+	assert.NoError(t, err)
+
+	// verify
+	assert.NotContains(t, res.Spec.Args, "--metrics-addr")
+	assert.NotContains(t, res.Spec.Args, "--metrics-level")
+}
