948c8ece74c9c377b927e8d070f185483268f702
Yuri Sa <yurimsa@gmail.com>
2022-02-10 13:08:22 +0100

    Removed empty lines

--- pkg/collector/upgrade/v0_43_0.go
+++ pkg/collector/upgrade/v0_43_0.go
@@ -48,20 +48,16 @@ func upgrade0_43_0(cl client.Client, otelcol *v1alpha1.OpenTelemetryCollector) (
 		if err != nil {
 			return otelcol, fmt.Errorf("couldn't upgrade to v0.43.0, failed to parse configuration: %w", err)
 		}
-
 		serviceConfig, ok := cfg["service"].(map[interface{}]interface{})
 		if !ok {
-
 			cfg["service"] = make(map[interface{}]interface{})
 			serviceConfig, _ = cfg["service"].(map[interface{}]interface{})
 		}
-
 		telemetryConfig, ok := serviceConfig["telemetry"].(map[interface{}]interface{})
 		if !ok {
 			serviceConfig["telemetry"] = make(map[interface{}]interface{})
 			telemetryConfig, _ = serviceConfig["telemetry"].(map[interface{}]interface{})
 		}
-
 		metricsConfig, ok := telemetryConfig["metrics"].(map[interface{}]interface{})
 		if !ok {
 			telemetryConfig["metrics"] = make(map[interface{}]interface{})
@@ -80,23 +76,16 @@ func upgrade0_43_0(cl client.Client, otelcol *v1alpha1.OpenTelemetryCollector) (
 		}
 		cfg["service"] = serviceConfig
 		res, err := yaml.Marshal(cfg)
-
 		if err != nil {
 			return otelcol, fmt.Errorf("couldn't upgrade to v0.43.0, failed to marshall back configuration: %w", err)
 		}
-
 		otelcol.Spec.Config = string(res)
-
 		keys := make([]string, 0, len(foundMetricsArgs))
-
 		for k := range foundMetricsArgs {
 			keys = append(keys, k)
 		}
-
 		sort.Strings(keys)
-
 		otelcol.Status.Messages = append(otelcol.Status.Messages, fmt.Sprintf("upgrade to v0.43.0 dropped the deprecated metrics arguments "+"i.e. %v from otelcol custom resource otelcol.spec.args and adding them to otelcol.spec.config.service.telemetry.metrics, if no metrics arguments are configured already.", keys))
 	}
-
 	return otelcol, nil
 }
