d9ca106de2f514420c4b4be7f0cdcd734632bd43
binjip978 <pdp.eleven11@gmail.com>
2022-04-27 06:34:29 +0000

    Move replicas calculation in seperate function
    Signed-off-by: binjip978 <pdp.eleven11@gmail.com>

--- pkg/collector/reconcile/deployment.go
+++ pkg/collector/reconcile/deployment.go
@@ -24,6 +24,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	"github.com/open-telemetry/opentelemetry-operator/pkg/collector"
 	"github.com/open-telemetry/opentelemetry-operator/pkg/targetallocator"
 )
@@ -99,19 +100,7 @@ func expectedDeployments(ctx context.Context, params Params, expected []appsv1.D
 		}
 
 		if params.Instance.Spec.MaxReplicas != nil {
-			currentReplicas := existing.Status.Replicas
-			if params.Instance.Spec.Replicas != nil {
-				// if replicas (minReplicas from HPA perspective) is bigger than
-				// current status use it.
-				if *params.Instance.Spec.Replicas > currentReplicas {
-					currentReplicas = *params.Instance.Spec.Replicas
-				}
-				// honor hpa max replicas parameter
-				if currentReplicas > *params.Instance.Spec.MaxReplicas {
-					currentReplicas = *params.Instance.Spec.MaxReplicas
-				}
-			}
-
+			currentReplicas := currentReplicasWithHPA(params.Instance.Spec, existing.Status.Replicas)
 			updated.Spec.Replicas = &currentReplicas
 		}
 
@@ -159,4 +148,17 @@ func deleteDeployments(ctx context.Context, params Params, expected []appsv1.Dep
 	}
 
 	return nil
+}
+
+// currentReplicasWithHPA calculates deployment replicas if HPA is enabled.
+func currentReplicasWithHPA(spec v1alpha1.OpenTelemetryCollectorSpec, curr int32) int32 {
+	if curr < *spec.Replicas {
+		return *spec.Replicas
+	}
+
+	if curr > *spec.MaxReplicas {
+		return *spec.MaxReplicas
+	}
+
+	return curr
 }
