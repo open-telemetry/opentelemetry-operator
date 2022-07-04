4c96e3f7c4359d8f13531b8d201e66c2e9fd32e6
Mateusz "mat" Rumian <58699800+mat-rumian@users.noreply.github.com>
2022-07-04 16:53:03 +0200

    Fix Pod Mutation loop (#953)
    * chore(webhookhandler): check if pod already instrumented
    
    * chore(instrumentations): add check for volume and instrumentation env var value
    
    * chore(webhookhandler): add helper tests
    
    * chore(instrumentation): add helper tests
    
    * chore(instrumentation): update volume checking method
    
    * chore(lint): fix
    
    * chore(webhookhandler): move out pod instrumentation check
    
    * chore(instrumentation): add pod instrumentation inject check
    
    * chore(instrumentation): clean up

--- pkg/instrumentation/python.go
+++ pkg/instrumentation/python.go
@@ -54,7 +54,9 @@ func injectPythonSDK(logger logr.Logger, pythonSpec v1alpha1.Python, pod corev1.
 			logger.Info("Skipping Python SDK injection, the container defines PYTHONPATH env var value via ValueFrom", "container", container.Name)
 			return pod
 		}
+
 		container.Env[idx].Value = fmt.Sprintf("%s:%s:%s", pythonPathPrefix, container.Env[idx].Value, pythonPathSuffix)
+
 	}
 
 	// Set OTEL_TRACES_EXPORTER to HTTP exporter if not set by user because it is what our autoinstrumentation supports.
@@ -72,7 +74,7 @@ func injectPythonSDK(logger logr.Logger, pythonSpec v1alpha1.Python, pod corev1.
 	})
 
 	// We just inject Volumes and init containers for the first processed container
-	if IsInitContainerMissing(pod) {
+	if isInitContainerMissing(pod) {
 		pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
 			Name: volumeName,
 			VolumeSource: corev1.VolumeSource{
