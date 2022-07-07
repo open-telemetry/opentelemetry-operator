aeb4a97d200a03db739dd969b0ab4980adc3fc3c
Kevin Earls <kearls@redhat.com>
2022-07-07 14:34:01 +0200

    Document that this script is not needed for minikube
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- hack/install-metrics-server.sh
+++ hack/install-metrics-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Install metrics-server for autoscale tests
+# Install metrics-server on kind clusters for autoscale tests.  Note: This is not needed for minikube,
+# you can just add --addons "metrics-server" to the start command.
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl patch deployment -n kube-system metrics-server --type "json" -p '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": --kubelet-insecure-tls}]'
 kubectl wait --for=condition=available deployment/metrics-server -n kube-system  --timeout=5m
