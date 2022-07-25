051bf705e0db289b671975110f9022ba7153a270
Kevin Earls <kearls@redhat.com>
2022-07-25 11:09:21 +0200

    Change Horizontal Pod Autoscaler to scale on OpenTelemetry Collector … (#984)
    * Change Horizontal Pod Autoscaler to scale on OpenTelemetry Collector object
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Update doc; move upgrade code out of reconciler
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Fixed doc
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Respond to comments, implement test
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * respond to comments
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- apis/v1alpha1/opentelemetrycollector_types.go
+++ apis/v1alpha1/opentelemetrycollector_types.go
@@ -33,9 +33,13 @@ type OpenTelemetryCollectorSpec struct {
 	// +optional
 	Args map[string]string `json:"args,omitempty"`
 
-	// Replicas is the number of pod instances for the underlying OpenTelemetry Collector
+	// Replicas is the number of pod instances for the underlying OpenTelemetry Collector. Set this if your are not using autoscaling
 	// +optional
 	Replicas *int32 `json:"replicas,omitempty"`
+
+	// MinReplicas sets a lower bound to the autoscaling feature.  Set this if your are using autoscaling. It must be at least 1
+	// +optional
+	MinReplicas *int32 `json:"minReplicas,omitempty"`
 
 	// MaxReplicas sets an upper bound to the autoscaling feature. If MaxReplicas is set autoscaling is enabled.
 	// +optional
