c871495d1c0a31e431b111d318c3a971ec82b46c
Mateusz Rumian <mrumian@sumologic.com>
2022-08-01 13:04:05 +0200

    chore(instrumentation): get back to grpc exporter

--- autoinstrumentation/nodejs/src/autoinstrumentation.ts
+++ autoinstrumentation/nodejs/src/autoinstrumentation.ts
@@ -1,5 +1,5 @@
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 
 import { NodeSDK } from '@opentelemetry/sdk-node';
 
