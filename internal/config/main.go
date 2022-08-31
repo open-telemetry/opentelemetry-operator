888aeeb212fa1820cec0779fc5d13043e1400f34
Kevin Earls <kearls@redhat.com>
2022-08-31 11:01:28 +0200

    Change log message to V(2), be sure to pass strings so it doesn't panic
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- internal/config/main.go
+++ internal/config/main.go
@@ -140,7 +140,7 @@ func (c *Config) AutoDetect() error {
 		return err
 	}
 	c.autoscalingVersion = hpaVersion
-	c.logger.Info("In Autodetect, Set HPA version to [", c.autoscalingVersion, "] from [", hpaVersion, "]")
+	c.logger.V(2).Info("In Autodetect, Set HPA version to [", c.autoscalingVersion.String(), "] from [", hpaVersion.String(), "]")
 
 	return nil
 }
