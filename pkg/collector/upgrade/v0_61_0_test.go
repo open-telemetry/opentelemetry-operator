3fa287b4b7692cbb993759e096a33aaedfe72786
Benedikt Bongartz <bongartz@klimlive.de>
2022-09-21 18:47:45 +0200

    upgrade routine for jaeger remote sampling
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

---
+++ pkg/collector/upgrade/v0_61_0_test.go
@@ -0,0 +1,65 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package upgrade_test
+
+import (
+	"context"
+	_ "embed"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/record"
+
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
+	"github.com/open-telemetry/opentelemetry-operator/internal/version"
+	"github.com/open-telemetry/opentelemetry-operator/pkg/collector/upgrade"
+)
+
+var (
+	//go:embed v0_61_0-input.yaml
+	input string
+	//go:embed v0_61_0-output.yaml
+	output string
+)
+
+func Test0_61_0Upgrade(t *testing.T) {
+	collectorInstance := v1alpha1.OpenTelemetryCollector{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "OpenTelemetryCollector",
+			APIVersion: "v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "otel-my-instance",
+			Namespace: "somewhere",
+		},
+		Spec: v1alpha1.OpenTelemetryCollectorSpec{
+			Config: input,
+		},
+	}
+
+	collectorInstance.Status.Version = "0.60.0"
+	//Test to remove port and change endpoint value.
+	versionUpgrade := &upgrade.VersionUpgrade{
+		Log:      logger,
+		Version:  version.Get(),
+		Client:   k8sClient,
+		Recorder: record.NewFakeRecorder(upgrade.RecordBufferSize),
+	}
+
+	upgradedInstance, err := versionUpgrade.ManagedInstance(context.Background(), collectorInstance)
+	assert.NoError(t, err)
+	assert.Equal(t, output, upgradedInstance.Spec.Config)
+}
