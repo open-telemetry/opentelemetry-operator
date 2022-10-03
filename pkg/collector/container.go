18f80443d3e303cacdb29dbb1be872818b2e53a5
Jacob Aronoff <jaronoff97@users.noreply.github.com>
2022-10-03 14:20:31 -0400

    Update pkg/collector/container.go
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>

--- pkg/collector/container.go
+++ pkg/collector/container.go
@@ -80,7 +80,10 @@ func Container(cfg config.Config, logger logr.Logger, otelcol v1alpha1.OpenTelem
 
 	if otelcol.Spec.TargetAllocator.Enabled {
 		// We need to add a SHARD here so the collector is able to keep targets after the hashmod operation which is
-		// added by default by the operator generator.
+		// added by default by the Prometheus operator's config generator.
+		// All collector instances use SHARD == 0 as they only receive targets
+		// allocated to them and should not use the Prometheus hashmod-based
+		// allocation.
 		envVars = append(envVars, corev1.EnvVar{
 			Name:  "SHARD",
 			Value: "0",
