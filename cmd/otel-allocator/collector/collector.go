77673ab265c3c4c0f8675e28fe68428d5c04df2e
Kristina Pathak <kristina.pathak@lightstep.com>
2022-10-03 01:22:38 -0700

    Fix lint issues in target allocator (#1090)
    * fix lint issues in target allocator files
    
    squash of all current commits on this branch
    
    * remove addtl golangci config
    
    * remove version.txt

--- cmd/otel-allocator/collector/collector.go
+++ cmd/otel-allocator/collector/collector.go
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package collector
 
 import (
@@ -108,7 +122,7 @@ func runWatch(ctx context.Context, k *Client, c <-chan watch.Event, collectorMap
 				return "no event"
 			}
 
-			switch event.Type {
+			switch event.Type { //nolint:exhaustive
 			case watch.Added:
 				collectorMap[pod.Name] = allocation.NewCollector(pod.Name)
 			case watch.Deleted:
--- cmd/otel-allocator/watcher/main.go
+++ cmd/otel-allocator/watcher/main.go
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package watcher
 
 import (
@@ -59,7 +73,7 @@ func NewWatcher(logger logr.Logger, config config.CLIConfig, allocator allocatio
 	watcher.watchers = append(watcher.watchers, &fileWatcher)
 
 	if *config.PromCRWatcherConf.Enabled {
-		promWatcher, err := newCRDMonitorWatcher(logger, config)
+		promWatcher, err := newCRDMonitorWatcher(config)
 		if err != nil {
 			return nil, err
 		}
