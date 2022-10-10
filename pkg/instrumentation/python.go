d703287d5c9b82d294bab7efd08ea3703438f06a
Avadhut Pisal <avadhutpisal47@gmail.com>
2022-10-10 20:34:31 +0530

    fixes go lint issue

--- pkg/instrumentation/python.go
+++ pkg/instrumentation/python.go
@@ -17,8 +17,9 @@ package instrumentation
 import (
 	"fmt"
 
-	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
+
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 )
 
 const (
