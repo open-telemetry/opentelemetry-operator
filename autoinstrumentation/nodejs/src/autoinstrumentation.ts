d36fff326911f9348a0ac68f2f227f48f50b2c6c
sairam.natarajan <sarpavur@cisco.com>
2022-10-27 12:02:34 -0700

    Add more resource detectors

--- autoinstrumentation/nodejs/src/autoinstrumentation.ts
+++ autoinstrumentation/nodejs/src/autoinstrumentation.ts
@@ -1,6 +1,18 @@
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { getNodeAutoResourceDetectors } from '@opentelemetry/auto-resource-detectors-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { alibabaCloudEcsDetector } from '@opentelemetry/resource-detector-alibaba-cloud';
+import {
+    awsBeanstalkDetector,
+    awsEc2Detector,
+    awsEcsDetector,
+    awsEksDetector,
+    awsLambdaDetector
+} from '@opentelemetry/resource-detector-aws';
+import { containerDetector } from '@opentelemetry/resource-detector-container';
+import { gcpDetector } from '@opentelemetry/resource-detector-gcp';
+import { gitHubDetector } from '@opentelemetry/resource-detector-github';
+import { browserDetector, envDetector, hostDetector, osDetector, processDetector } from '@opentelemetry/resources';
+import { instanaAgentDetector } from '@opentelemetry/resource-detector-instana';
 
 import { NodeSDK } from '@opentelemetry/sdk-node';
 
@@ -8,7 +20,29 @@ const sdk = new NodeSDK({
     autoDetectResources: true,
     instrumentations: [getNodeAutoInstrumentations()],
     traceExporter: new OTLPTraceExporter(),
-    resourceDetectors: [getNodeAutoResourceDetectors()],
+    resourceDetectors:
+        [
+            // Standard resource detectors.
+            browserDetector,
+            containerDetector,
+            envDetector,
+            hostDetector,
+            osDetector,
+            processDetector,
+
+            // Cloud resource detectors.
+            alibabaCloudEcsDetector,
+            awsBeanstalkDetector,
+            awsEc2Detector,
+            awsEcsDetector,
+            awsEksDetector,
+            awsLambdaDetector,
+            gcpDetector,
+            gitHubDetector,
+
+            // Agent resource detectors.
+            instanaAgentDetector,
+        ],
 });
 
 sdk.start();
