a4cbaaba82a28417c607dffd12e5263b30236d50
Mike Dame <mikedame@google.com>
2022-11-10 15:21:06 +0000

    Add function to parse spec.ConfigMap to a string

--- pkg/collector/adapters/config_from.go
+++ pkg/collector/adapters/config_from.go
@@ -16,9 +16,14 @@
 package adapters
 
 import (
+	"context"
 	"errors"
 
+	"github.com/go-logr/logr"
 	"gopkg.in/yaml.v2"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var (
@@ -35,4 +40,21 @@ func ConfigFromString(configStr string) (map[interface{}]interface{}, error) {
 	}
 
 	return config, nil
+}
+
+// GetConfigString returns the string value for the Collector, whether that is from spec.config as a string literal
+// or from spec.configMap as stored in a ConfigMap. The value can be used later in ConfigFromString.
+func GetConfigString(client client.Client, logger logr.Logger, configStr string, configMap v1.ConfigMapKeySelector, namespace string) string {
+	// the webhook validates that only one of spec.Config or spec.ConfigMap will be set, so we can assume that here
+	if len(configStr) > 0 {
+		return configStr
+	}
+
+	obj := &v1.ConfigMap{}
+	err := client.Get(context.TODO(), types.NamespacedName{Namespace: namespace, Name: configMap.Name}, obj)
+	if err != nil {
+		logger.Error(err, "error getting configmap")
+		return ""
+	}
+	return obj.Data[configMap.Key]
 }
