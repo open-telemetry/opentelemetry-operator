5e4d3995d481ebea619993acefeccffcafdba1f4
Kevin Earls <kearls@redhat.com>
2022-12-01 10:48:51 +0100

    Ignore reconcile errors that occur because a pod is being terminated (#1233)
    * Ignore reconcile errors that occur because a pod is being terminated
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Appease the all powerfull linter
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Change behavior to end reconcile loop if pod has been terminated
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Print a log message if we exit the reconciler loop
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Look for NamespaceTerminatingCause
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Appease the almighty linter
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Fix log message
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    * Skip flaky test
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    
    Signed-off-by: Kevin Earls <kearls@redhat.com>
    Co-authored-by: Ben B <bongartz@klimlive.de>

--- controllers/opentelemetrycollector_controller.go
+++ controllers/opentelemetrycollector_controller.go
@@ -168,6 +168,11 @@ func (r *OpenTelemetryCollectorReconciler) Reconcile(ctx context.Context, req ct
 func (r *OpenTelemetryCollectorReconciler) RunTasks(ctx context.Context, params reconcile.Params) error {
 	for _, task := range r.tasks {
 		if err := task.Do(ctx, params); err != nil {
+			// If we get an error that occurs because a pod is being terminated, then exit this loop
+			if apierrors.IsForbidden(err) && apierrors.HasStatusCause(err, corev1.NamespaceTerminatingCause) {
+				r.log.V(2).Info("Exiting reconcile loop because namespace is being terminated", "namespace", params.Instance.Namespace)
+				return nil
+			}
 			r.log.Error(err, fmt.Sprintf("failed to reconcile %s", task.Name))
 			if task.BailOnError {
 				return err
