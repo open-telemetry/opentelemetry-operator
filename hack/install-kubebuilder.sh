2032a0224c1fb204d1ec6df4e8fe1771cea4ca27
fengshunli <1171313930@qq.com>
2023-02-04 14:38:07 +0800

    [chore] Upgrade kubebuilder and kustomize version
    Signed-off-by: fengshunli <1171313930@qq.com>

--- hack/install-kubebuilder.sh
+++ hack/install-kubebuilder.sh
@@ -2,6 +2,6 @@
 
 os=$(go env GOOS)
 arch=$(go env GOARCH)
-curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.3.1/kubebuilder_2.3.1_${os}_${arch}.tar.gz | tar -xz -C /tmp/
-sudo mv /tmp/kubebuilder_2.3.1_${os}_${arch} /usr/local/kubebuilder
-export PATH=$PATH:/usr/local/kubebuilder/bin
+version=3.9.0
+sudo curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${version}/kubebuilder_${os}_${arch} -o /usr/local/bin/kubebuilder
+export PATH=$PATH:/usr/local/bin
