c704480ad0547eaa2d66c4f7e41618ed43a77af1
Avadhut Pisal <avadhutpisal47@gmail.com>
2023-02-14 22:02:02 +0530

    Add Operator-Opamp-Bridge build and version info to Operator (#1467)
    * adds operator-opamp-bridge build info to operator
    
    * adds changelog

--- internal/config/options.go
+++ internal/config/options.go
@@ -41,6 +41,7 @@ type options struct {
 	collectorConfigMapEntry        string
 	targetAllocatorConfigMapEntry  string
 	targetAllocatorImage           string
+	operatorOpAMPBridgeImage       string
 	onPlatformChange               changeHandler
 	labelsFilter                   []string
 	platform                       platformStore
@@ -63,7 +64,11 @@ func WithTargetAllocatorImage(s string) Option {
 		o.targetAllocatorImage = s
 	}
 }
-
+func WithOperatorOpAMPBridgeImage(s string) Option {
+	return func(o *options) {
+		o.operatorOpAMPBridgeImage = s
+	}
+}
 func WithCollectorImage(s string) Option {
 	return func(o *options) {
 		o.collectorImage = s
