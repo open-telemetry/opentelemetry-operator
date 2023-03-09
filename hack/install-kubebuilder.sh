df38ddf596cf4ef6fe2f4eaddd825a21068e9fa1
Pavol Loffay <p.loffay@gmail.com>
2023-03-09 13:21:03 +0100

    Remove unused kubebuilder
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- hack/install-kubebuilder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-os=$(go env GOOS)
-arch=$(go env GOARCH)
-curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.3.1/kubebuilder_2.3.1_${os}_${arch}.tar.gz | tar -xz -C /tmp/
-sudo mv /tmp/kubebuilder_2.3.1_${os}_${arch} /usr/local/kubebuilder
-export PATH=$PATH:/usr/local/kubebuilder/bin
