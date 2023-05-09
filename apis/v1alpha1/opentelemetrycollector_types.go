2e537f04a7bbc35b7c05f61b81ae559da0018204
YUANFAN PENG <yuanpeng@cisco.com>
2023-05-09 01:24:40 -0700

    Add liveness missing configs (#1686)
    * add configs for liveness probe
    
    * add testcase
    
    * add change log
    
    * make bundle
    
    * make bundle
    
    * make bundle
    
    * make bundle USER=open-telemetry VERSION=0.75.0
    
    * update local controller-gen version
    
    * add missing newline
    
    * address comment revert file
    
    * add comments
    
    * add validator, make bundle
    
    * validator should ignore empty value, add testcases
    
    * Revert "validator should ignore empty value, add testcases"
    
    This reverts commit 2b0dd506dadd0978d5d5670c2b92386999d0a086.
    
    * Revert "add validator, make bundle"
    
    This reverts commit b65f1727e350ac8aaaf8077e81db381d4bcec964.
    
    * use webhook specCrd validator
    
    * make api-docs
    
    * adjust error msg
    
    * fix type
    
    * revert file
    
    * fix typo
    
    * add logs for probe config error.
    
    * Update comment in apis/v1alpha1/opentelemetrycollector_types.go
    
    Co-authored-by: Jacob Aronoff <jaronoff97@users.noreply.github.com>
    
    * make bundle
    
    * make api docs
    
    ---------
    
    Co-authored-by: Jacob Aronoff <jaronoff97@users.noreply.github.com>

--- apis/v1alpha1/opentelemetrycollector_types.go
+++ apis/v1alpha1/opentelemetrycollector_types.go
@@ -179,6 +179,10 @@ type OpenTelemetryCollectorSpec struct {
 	// Duration in seconds the pod needs to terminate gracefully upon probe failure.
 	// +optional
 	TerminationGracePeriodSeconds *int64 `json:"terminationGracePeriodSeconds,omitempty"`
+	// Liveness config for the OpenTelemetry Collector except the probe handler which is auto generated from the health extension of the collector.
+	// It is only effective when healthcheckextension is configured in the OpenTelemetry Collector pipeline.
+	// +optional
+	LivenessProbe *Probe `json:"livenessProbe,omitempty"`
 }
 
 // OpenTelemetryTargetAllocator defines the configurations for the Prometheus target allocator.
@@ -312,6 +316,44 @@ type AutoscalerSpec struct {
 	// +optional
 	// TargetMemoryUtilization sets the target average memory utilization across all replicas
 	TargetMemoryUtilization *int32 `json:"targetMemoryUtilization,omitempty"`
+}
+
+// Probe defines the OpenTelemetry's pod probe config. Only Liveness probe is supported currently.
+type Probe struct {
+	// Number of seconds after the container has started before liveness probes are initiated.
+	// Defaults to 0 seconds. Minimum value is 0.
+	// More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#container-probes
+	// +optional
+	InitialDelaySeconds *int32 `json:"initialDelaySeconds,omitempty"`
+	// Number of seconds after which the probe times out.
+	// Defaults to 1 second. Minimum value is 1.
+	// More info: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle#container-probes
+	// +optional
+	TimeoutSeconds *int32 `json:"timeoutSeconds,omitempty"`
+	// How often (in seconds) to perform the probe.
+	// Default to 10 seconds. Minimum value is 1.
+	// +optional
+	PeriodSeconds *int32 `json:"periodSeconds,omitempty"`
+	// Minimum consecutive successes for the probe to be considered successful after having failed.
+	// Defaults to 1. Must be 1 for liveness and startup. Minimum value is 1.
+	// +optional
+	SuccessThreshold *int32 `json:"successThreshold,omitempty"`
+	// Minimum consecutive failures for the probe to be considered failed after having succeeded.
+	// Defaults to 3. Minimum value is 1.
+	// +optional
+	FailureThreshold *int32 `json:"failureThreshold,omitempty"`
+	// Optional duration in seconds the pod needs to terminate gracefully upon probe failure.
+	// The grace period is the duration in seconds after the processes running in the pod are sent
+	// a termination signal and the time when the processes are forcibly halted with a kill signal.
+	// Set this value longer than the expected cleanup time for your process.
+	// If this value is nil, the pod's terminationGracePeriodSeconds will be used. Otherwise, this
+	// value overrides the value provided by the pod spec.
+	// Value must be non-negative integer. The value zero indicates stop immediately via
+	// the kill signal (no opportunity to shut down).
+	// This is a beta field and requires enabling ProbeTerminationGracePeriod feature gate.
+	// Minimum value is 1. spec.terminationGracePeriodSeconds is used if unset.
+	// +optional
+	TerminationGracePeriodSeconds *int64 `json:"terminationGracePeriodSeconds,omitempty"`
 }
 
 func init() {
