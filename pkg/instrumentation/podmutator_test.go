4de0d208a50394fe166dada924cdb4994c7e81b4
Ben B <bongartz@klimlive.de>
2023-05-11 17:15:20 +0200

    set default resource limits for instrumentation init containers (#1571)
    * set default resource limits for instrumentation init containers
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * expose resource requirements for instrumentation init containers
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    ---------
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- pkg/instrumentation/podmutator_test.go
+++ pkg/instrumentation/podmutator_test.go
@@ -72,6 +72,7 @@ func TestMutatePod(t *testing.T) {
 								Value: "false",
 							},
 						},
+						Resources: testResourceRequirements,
 					},
 					Env: []corev1.EnvVar{
 						{
@@ -141,6 +142,7 @@ func TestMutatePod(t *testing.T) {
 								Name:      volumeName,
 								MountPath: "/otel-auto-instrumentation",
 							}},
+							Resources: testResourceRequirements,
 						},
 					},
 					Containers: []corev1.Container{
