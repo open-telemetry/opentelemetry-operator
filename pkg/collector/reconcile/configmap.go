6461823da1efdc530e3903dbaf5494af3845cea9
Anusha Narapureddy <anusha_narapureddy@apple.com>
2023-05-30 17:40:43 -0700

    comment for unescaping TA config

--- pkg/collector/reconcile/configmap.go
+++ pkg/collector/reconcile/configmap.go
@@ -103,6 +103,8 @@ func desiredTAConfigMap(params Params) (corev1.ConfigMap, error) {
 		labels["app.kubernetes.io/version"] = "latest"
 	}
 
+	// Collector supports environment variable substitution, but the TA does not.
+	// TA ConfigMap should have a single "$", as it does not support env var substitution
 	prometheusReceiverConfig, err := ta.UnescapeDollarSignsInPromConfig(params.Instance.Spec.Config)
 	if err != nil {
 		return corev1.ConfigMap{}, err
