1a09e7323abd1dce41133c62445cd7d6d35c1232
Avadhut Pisal <avadhutpisal47@gmail.com>
2023-08-02 20:25:24 +0530

    Add managementState field to collector crd spec (#1888)
    * adds managementState field to collector crd spec
    
    * changes e2e test case
    
    * cover new case in e2e test case
    
    * fix typo
    
    * fix e2e test cases
    
    * fix e2e test case
    
    * add assertion on config map state

--- pkg/collector/upgrade/upgrade_test.go
+++ pkg/collector/upgrade/upgrade_test.go
@@ -48,7 +48,7 @@ func TestShouldUpgradeAllToLatestBasedOnUpgradeStrategy(t *testing.T) {
 		t.Run("spec.UpgradeStrategy = "+string(tt.strategy), func(t *testing.T) {
 			// prepare
 			nsn := types.NamespacedName{Name: "my-instance", Namespace: "default"}
-			existing := makeOtelcol(nsn)
+			existing := makeOtelcol(nsn, v1alpha1.ManagementStateManaged)
 			err := k8sClient.Create(context.Background(), &existing)
 			require.NoError(t, err)
 
@@ -95,7 +95,7 @@ func TestUpgradeUpToLatestKnownVersion(t *testing.T) {
 		t.Run(tt.desc, func(t *testing.T) {
 			// prepare
 			nsn := types.NamespacedName{Name: "my-instance", Namespace: "default"}
-			existing := makeOtelcol(nsn)
+			existing := makeOtelcol(nsn, v1alpha1.ManagementStateManaged)
 			existing.Status.Version = tt.v
 
 			currentV := version.Get()
@@ -117,20 +117,23 @@ func TestUpgradeUpToLatestKnownVersion(t *testing.T) {
 }
 
 func TestVersionsShouldNotBeChanged(t *testing.T) {
+	nsn := types.NamespacedName{Name: "my-instance", Namespace: "default"}
 	for _, tt := range []struct {
 		desc            string
 		v               string
 		expectedV       string
 		failureExpected bool
+		managementState v1alpha1.ManagementStateType
 	}{
-		{"new-instance", "", "", false},
-		{"newer-than-our-newest", "100.0.0", "100.0.0", false},
-		{"unparseable", "unparseable", "unparseable", true},
+		{"new-instance", "", "", false, v1alpha1.ManagementStateManaged},
+		{"newer-than-our-newest", "100.0.0", "100.0.0", false, v1alpha1.ManagementStateManaged},
+		{"unparseable", "unparseable", "unparseable", true, v1alpha1.ManagementStateManaged},
+		// Ignore unmanaged instances
+		{"unmanaged-instance", "1.0.0", "1.0.0", false, v1alpha1.ManagementStateUnmanaged},
 	} {
 		t.Run(tt.desc, func(t *testing.T) {
 			// prepare
-			nsn := types.NamespacedName{Name: "my-instance", Namespace: "default"}
-			existing := makeOtelcol(nsn)
+			existing := makeOtelcol(nsn, tt.managementState)
 			existing.Status.Version = tt.v
 
 			currentV := version.Get()
@@ -157,8 +160,11 @@ func TestVersionsShouldNotBeChanged(t *testing.T) {
 	}
 }
 
-func makeOtelcol(nsn types.NamespacedName) v1alpha1.OpenTelemetryCollector {
+func makeOtelcol(nsn types.NamespacedName, managementState v1alpha1.ManagementStateType) v1alpha1.OpenTelemetryCollector {
 	return v1alpha1.OpenTelemetryCollector{
+		Spec: v1alpha1.OpenTelemetryCollectorSpec{
+			ManagementState: managementState,
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      nsn.Name,
 			Namespace: nsn.Namespace,
