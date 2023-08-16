94a8d67a513219afaa572cfd6c75c0c6c8157e57
Pavol Loffay <p.loffay@gmail.com>
2023-08-16 15:39:18 +0200

    Fix
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- internal/config/main_test.go
+++ internal/config/main_test.go
@@ -85,7 +85,7 @@ func TestAutoDetectInBackground(t *testing.T) {
 	}
 	cfg := config.New(
 		config.WithAutoDetect(mock),
-		config.WithAutoDetectFrequency(100*time.Millisecond),
+		config.WithAutoDetectFrequency(500*time.Second),
 	)
 
 	// sanity check
