dc42afed680d7a76210095afa11527be5de84abc
Israel Blancas <iblancasa@gmail.com>
2023-09-11 18:48:30 +0200

    Move import
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- pkg/collector/reconcile/opentelemetry.go
+++ pkg/collector/reconcile/opentelemetry.go
@@ -23,12 +23,11 @@ import (
 	"github.com/open-telemetry/opentelemetry-operator/internal/manifests"
 	"github.com/open-telemetry/opentelemetry-operator/internal/manifests/collector"
 	"github.com/open-telemetry/opentelemetry-operator/internal/naming"
+	collectorupgrade "github.com/open-telemetry/opentelemetry-operator/pkg/collector/upgrade"
 
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	collectorupgrade "github.com/open-telemetry/opentelemetry-operator/pkg/collector/upgrade"
 
 	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	"github.com/open-telemetry/opentelemetry-operator/internal/version"
