88bfe1c03a50b54dc5d66640c3213ee658548719
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2023-09-21 15:50:21 +0000

    Refactor target allocator config handling (#1957)
    * Refactor target allocator config handling
    
    * Move TargetAllocator flags to a separate file
    
    * Fix yaml annotation on the TA config struct
    
    * Clearly separate loading config from CLI and file
    
    * Use an explicit flag set for target allocator flags
    
    * Add changelog entry
    
    * Pass config by value to PrometheusCRWatcher

--- cmd/otel-allocator/watcher/promOperator.go
+++ cmd/otel-allocator/watcher/promOperator.go
@@ -37,13 +37,13 @@ import (
 	allocatorconfig "github.com/open-telemetry/opentelemetry-operator/cmd/otel-allocator/config"
 )
 
-func NewPrometheusCRWatcher(logger logr.Logger, cfg allocatorconfig.Config, cliConfig allocatorconfig.CLIConfig) (*PrometheusCRWatcher, error) {
-	mClient, err := monitoringclient.NewForConfig(cliConfig.ClusterConfig)
+func NewPrometheusCRWatcher(logger logr.Logger, cfg allocatorconfig.Config) (*PrometheusCRWatcher, error) {
+	mClient, err := monitoringclient.NewForConfig(cfg.ClusterConfig)
 	if err != nil {
 		return nil, err
 	}
 
-	clientset, err := kubernetes.NewForConfig(cliConfig.ClusterConfig)
+	clientset, err := kubernetes.NewForConfig(cfg.ClusterConfig)
 	if err != nil {
 		return nil, err
 	}
@@ -80,7 +80,7 @@ func NewPrometheusCRWatcher(logger logr.Logger, cfg allocatorconfig.Config, cliC
 		informers:              monitoringInformers,
 		stopChannel:            make(chan struct{}),
 		configGenerator:        generator,
-		kubeConfigPath:         cliConfig.KubeConfigFilePath,
+		kubeConfigPath:         cfg.KubeConfigFilePath,
 		serviceMonitorSelector: servMonSelector,
 		podMonitorSelector:     podMonSelector,
 	}, nil
