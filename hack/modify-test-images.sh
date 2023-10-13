f71b489963c75b174b4dae4b5f3ff9b38c8c91c0
Avadhut Pisal <avadhutpisal47@gmail.com>
2023-10-13 20:40:39 +0530

    add local e2e image entry for opampbridge in hack

--- hack/modify-test-images.sh
+++ hack/modify-test-images.sh
@@ -7,3 +7,5 @@ ${SED_BIN} -i "s#local/opentelemetry-operator-targetallocator:e2e#${TARGETALLOCA
 ${SED_BIN} -i "s#local/opentelemetry-operator-targetallocator:e2e#${TARGETALLOCATOR_IMG}#g" tests/e2e/prometheus-config-validation/*.yaml
 
 ${SED_BIN} -i "s#local/opentelemetry-operator:e2e#${OPERATOR_IMG}#g" tests/e2e-multi-instrumentation/*.yaml
+
+${SED_BIN} -i "s#local/opentelemetry-operator-opamp-bridge:e2e#${OPERATOROPAMPBRIDGE_IMG}#g" tests/e2e/opampbridge/*.yaml
