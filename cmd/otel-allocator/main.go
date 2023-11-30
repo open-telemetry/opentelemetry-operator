a0bb53c2e6dbd9e4dbb69f4203535108a324a1fe
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2023-11-30 11:00:01 +0100

    Remove configuration hot reloading from target allocator

--- cmd/otel-allocator/main.go
+++ cmd/otel-allocator/main.go
@@ -54,7 +54,6 @@ func main() {
 		allocator        allocation.Allocator
 		discoveryManager *discovery.Manager
 		collectorWatcher *collector.Client
-		fileWatcher      allocatorWatcher.Watcher
 		promWatcher      allocatorWatcher.Watcher
 		targetDiscoverer *target.Discoverer
 
@@ -65,7 +64,7 @@ func main() {
 		interrupts      = make(chan os.Signal, 1)
 		errChan         = make(chan error)
 	)
-	cfg, configFilePath, err := config.Load()
+	cfg, err := config.Load()
 	if err != nil {
 		fmt.Printf("Failed to load config: %v", err)
 		os.Exit(1)
@@ -99,13 +98,6 @@ func main() {
 		setupLog.Error(collectorWatcherErr, "Unable to initialize collector watcher")
 		os.Exit(1)
 	}
-	if cfg.ReloadConfig {
-		fileWatcher, err = allocatorWatcher.NewFileWatcher(setupLog.WithName("file-watcher"), configFilePath)
-		if err != nil {
-			setupLog.Error(err, "Can't start the file watcher")
-			os.Exit(1)
-		}
-	}
 	signal.Notify(interrupts, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	defer close(interrupts)
 
@@ -126,21 +118,6 @@ func main() {
 				promWatcherErr := promWatcher.Close()
 				if promWatcherErr != nil {
 					setupLog.Error(promWatcherErr, "prometheus watcher failed to close")
-				}
-			})
-	}
-	if cfg.ReloadConfig {
-		runGroup.Add(
-			func() error {
-				fileWatcherErr := fileWatcher.Watch(eventChan, errChan)
-				setupLog.Info("File watcher exited")
-				return fileWatcherErr
-			},
-			func(_ error) {
-				setupLog.Info("Closing file watcher")
-				fileWatcherErr := fileWatcher.Close()
-				if fileWatcherErr != nil {
-					setupLog.Error(fileWatcherErr, "file watcher failed to close")
 				}
 			})
 	}
