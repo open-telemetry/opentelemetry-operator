391efaa5fd23a91dba9728f7a96f317c76c589b5
Jason Crouse <jason.crouse@lightstep.com>
2023-12-04 12:51:54 -0500

    rename file to headers.go, s/ToHttpHeader/ToHTTPHeader

--- cmd/operator-opamp-bridge/config/headers.go
+++ cmd/operator-opamp-bridge/config/headers.go
@@ -4,7 +4,7 @@ import "net/http"
 
 type Headers map[string]string
 
-func (h Headers) ToHttpHeader() http.Header {
+func (h Headers) ToHTTPHeader() http.Header {
 	newMap := make(map[string][]string)
 	for key, value := range h {
 		newMap[key] = []string{value}
