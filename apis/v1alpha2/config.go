ed7b81bc94f5df3a4c541d9dcd7659a9062c07e2
Pavol Loffay <p.loffay@gmail.com>
2023-12-15 16:45:33 +0100

    Fix
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- apis/v1alpha2/config.go
+++ apis/v1alpha2/config.go
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package v1alpha2
 
 import (
@@ -5,10 +19,10 @@ import (
 	"encoding/json"
 )
 
-// Values hold a map, with string as the key and either a string or a slice of strings as the value
+// Values represent parts of the config.
 type Values map[string]interface{}
 
-// DeepCopy indicate how to do a deep copy of Values type
+// DeepCopy indicate how to do a deep copy of Values type.
 func (v *Values) DeepCopy() *Values {
 	out := make(Values, len(*v))
 	for key, val := range *v {
@@ -25,6 +39,7 @@ func (v *Values) DeepCopy() *Values {
 	return &out
 }
 
+// Config encapsulates collector config.
 type Config struct {
 	cfg Values `json:"-"`
 }
@@ -32,7 +47,7 @@ type Config struct {
 var _ json.Marshaler = &Config{}
 var _ json.Unmarshaler = &Config{}
 
-// UnmarshalJSON implements an alternative parser for this field
+// UnmarshalJSON implements an alternative parser for this field.
 func (c *Config) UnmarshalJSON(b []byte) error {
 	var entries Values
 	d := json.NewDecoder(bytes.NewReader(b))
@@ -44,7 +59,7 @@ func (c *Config) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON specifies how to convert this object into JSON
+// MarshalJSON specifies how to convert this object into JSON.
 func (c *Config) MarshalJSON() ([]byte, error) {
 	if len(c.cfg) == 0 {
 		return []byte("{}"), nil
--- apis/v1alpha2/opentelemetrycollector_types.go
+++ apis/v1alpha2/opentelemetrycollector_types.go
@@ -100,9 +100,8 @@ type OpenTelemetryCollectorSpec struct {
 	// +optional
 	ImagePullPolicy v1.PullPolicy `json:"imagePullPolicy,omitempty"`
 	// Config is the raw JSON to be used as the collector's configuration. Refer to the OpenTelemetry Collector documentation for details.
-	// The empty objects e.g. batch: should be written as batch: {}
+	// The empty objects e.g. batch: should be written as batch: {}.
 	// +required
-	//Config string `json:"config,omitempty"`
 	// +kubebuilder:pruning:PreserveUnknownFields
 	Config Config `json:"config"`
 
