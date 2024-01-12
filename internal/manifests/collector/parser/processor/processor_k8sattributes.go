42e9d039d13f833f3d16e634d365bb38be5072d1
Israel Blancas <iblancasa@gmail.com>
2024-01-12 11:08:19 +0100

    Automate the creation of the permissions needed by k8sattributes. (#2396)
    * Automate the creation of the RBAC resources for the k8sattributes processor
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>
    
    * Add missing permission
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>
    
    * Add missing changelog
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>
    
    ---------
    
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

---
+++ internal/manifests/collector/parser/processor/processor_k8sattributes.go
@@ -0,0 +1,109 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package processor
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-logr/logr"
+	rbacv1 "k8s.io/api/rbac/v1"
+)
+
+var _ ProcessorParser = &K8sAttributesParser{}
+
+const (
+	parserNameK8sAttributes = "__k8sattributes"
+)
+
+// PrometheusExporterParser parses the configuration for k8sattributes processor.
+type K8sAttributesParser struct {
+	config map[interface{}]interface{}
+	logger logr.Logger
+	name   string
+}
+
+// NewK8sAttributesParser builds a new parser k8sattributes processor.
+func NewK8sAttributesParser(logger logr.Logger, name string, config map[interface{}]interface{}) ProcessorParser {
+	return &K8sAttributesParser{
+		logger: logger,
+		name:   name,
+		config: config,
+	}
+}
+
+// ParserName returns the name of this parser.
+func (o *K8sAttributesParser) ParserName() string {
+	return parserNameK8sAttributes
+}
+
+func (o *K8sAttributesParser) GetRBACRules() []rbacv1.PolicyRule {
+	var prs []rbacv1.PolicyRule
+
+	extractCfg, ok := o.config["extract"]
+	if !ok {
+		return prs
+	}
+
+	metadataCfg, ok := extractCfg.(map[interface{}]interface{})["metadata"]
+	if !ok {
+		return prs
+	}
+
+	metadata, ok := metadataCfg.([]interface{})
+	if !ok {
+		return prs
+	}
+
+	// This one needs to be added always
+	policy := rbacv1.PolicyRule{
+		APIGroups: []string{""},
+		Resources: []string{"pods", "namespaces"},
+		Verbs:     []string{"get", "watch", "list"},
+	}
+	prs = append(prs, policy)
+
+	for _, m := range metadata {
+		metadataField := fmt.Sprint(m)
+		if metadataField == "k8s.deployment.uid" || metadataField == "k8s.deployment.name" {
+			prs = append(prs,
+				rbacv1.PolicyRule{
+					APIGroups: []string{"apps"},
+					Resources: []string{"replicasets"},
+					Verbs:     []string{"get", "watch", "list"},
+				},
+				rbacv1.PolicyRule{
+					APIGroups: []string{"extensions"},
+					Resources: []string{"replicasets"},
+					Verbs:     []string{"get", "watch", "list"},
+				},
+			)
+		} else if strings.Contains(metadataField, "k8s.node") {
+			prs = append(prs,
+				rbacv1.PolicyRule{
+					APIGroups: []string{""},
+					Resources: []string{"nodes"},
+					Verbs:     []string{"get", "watch", "list"},
+				},
+			)
+		}
+	}
+
+	return prs
+}
+
+func init() {
+	Register("k8sattributes", NewK8sAttributesParser)
+}
