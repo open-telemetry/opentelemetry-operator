5daa2df913010f23f2f5f333eaf75f365c8a17db
Jason Crouse <jason.crouse@lightstep.com>
2024-01-29 11:36:40 -0500

    add tests to confirm headers can be parsed from env variables

--- cmd/operator-opamp-bridge/config/config.go
+++ cmd/operator-opamp-bridge/config/config.go
@@ -269,11 +269,11 @@ func LoadFromFile(cfg *Config, configFile string) error {
 	if err != nil {
 		return err
 	}
-	envExpandedYaml := os.ExpandEnv(string(yamlFile))
-	if err = yaml.UnmarshalStrict([]byte(envExpandedYaml), cfg); err != nil {
+	envExpandedYaml := []byte(os.ExpandEnv(string(yamlFile)))
+	if err = yaml.UnmarshalStrict(envExpandedYaml, cfg); err != nil {
 		return fmt.Errorf("error unmarshaling YAML: %w", err)
 	}
-	if err = yaml.Unmarshal(yamlFile, cfg); err != nil {
+	if err = yaml.Unmarshal(envExpandedYaml, cfg); err != nil {
 		return fmt.Errorf("error unmarshaling YAML: %w", err)
 	}
 	return nil
