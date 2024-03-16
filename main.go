c5e6384d41ec2c22ff44d315115340f23f0f81a6
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2024-03-16 18:55:51 +0100

    Add controller for TargetAllocator CR

--- main.go
+++ main.go
@@ -384,6 +384,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	// TODO: Uncomment the line below to enable the Target Allocator controller
+	//if err = controllers.NewTargetAllocatorReconciler(
+	//	mgr.GetClient(),
+	//	mgr.GetScheme(),
+	//	mgr.GetEventRecorderFor("targetallocator"),
+	//	cfg,
+	//	ctrl.Log.WithName("controllers").WithName("TargetAllocator"),
+	//).SetupWithManager(mgr); err != nil {
+	//	setupLog.Error(err, "unable to create controller", "controller", "TargetAllocator")
+	//	os.Exit(1)
+	//}
+
 	if err = controllers.NewOpAMPBridgeReconciler(controllers.OpAMPBridgeReconcilerParams{
 		Client:   mgr.GetClient(),
 		Log:      ctrl.Log.WithName("controllers").WithName("OpAMPBridge"),
@@ -430,6 +442,11 @@ func main() {
 			setupLog.Error(err, "unable to create webhook", "webhook", "OpenTelemetryCollector")
 			os.Exit(1)
 		}
+		// TODO: Uncomment the line below to enable the Target Allocator webhook
+		//if err = otelv1alpha1.SetupTargetAllocatorWebhook(mgr, cfg, reviewer); err != nil {
+		//	setupLog.Error(err, "unable to create webhook", "webhook", "TargetAllocator")
+		//	os.Exit(1)
+		//}
 		if err = otelv1alpha1.SetupInstrumentationWebhook(mgr, cfg); err != nil {
 			setupLog.Error(err, "unable to create webhook", "webhook", "Instrumentation")
 			os.Exit(1)
