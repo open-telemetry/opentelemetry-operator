c0e1e55d92783b47f83e6bd2daf3f820f68c38db
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2024-03-28 11:02:35 +0100

    Promote operator.collector.rewritetargetallocator feature flag to stable

--- internal/manifests/collector/configmap_test.go
+++ internal/manifests/collector/configmap_test.go
@@ -17,11 +17,7 @@ package collector
 import (
 	"testing"
 
-	colfeaturegate "go.opentelemetry.io/collector/featuregate"
-
 	"github.com/stretchr/testify/assert"
-
-	"github.com/open-telemetry/opentelemetry-operator/pkg/featuregate"
 )
 
 func TestDesiredConfigMap(t *testing.T) {
@@ -71,111 +67,6 @@ service:
 		for k, expected := range expectedData {
 			assert.YAMLEq(t, expected, actual.Data[k])
 		}
-	})
-
-	t.Run("should return expected collector config map with http_sd_config if rewrite flag disabled", func(t *testing.T) {
-		err := colfeaturegate.GlobalRegistry().Set(featuregate.EnableTargetAllocatorRewrite.ID(), false)
-		assert.NoError(t, err)
-		t.Cleanup(func() {
-			_ = colfeaturegate.GlobalRegistry().Set(featuregate.EnableTargetAllocatorRewrite.ID(), true)
-		})
-		expectedLables["app.kubernetes.io/component"] = "opentelemetry-collector"
-		expectedLables["app.kubernetes.io/name"] = "test-collector"
-
-		expectedData := map[string]string{
-			"collector.yaml": `exporters:
-  debug:
-receivers:
-  jaeger:
-    protocols:
-      grpc: null
-  prometheus:
-    config:
-      scrape_configs:
-      - http_sd_configs:
-        - url: http://test-targetallocator:80/jobs/otel-collector/targets?collector_id=$POD_NAME
-        job_name: otel-collector
-        scrape_interval: 10s
-service:
-  pipelines:
-    metrics:
-      exporters:
-      - debug
-      processors: []
-      receivers:
-      - prometheus
-      - jaeger
-`,
-		}
-
-		param := deploymentParams()
-		param.OtelCol.Spec.TargetAllocator.Enabled = true
-		actual, err := ConfigMap(param)
-
-		assert.NoError(t, err)
-		assert.Equal(t, "test-collector", actual.GetName())
-		assert.Equal(t, expectedLables, actual.GetLabels())
-		assert.Equal(t, len(expectedData), len(actual.Data))
-		for k, expected := range expectedData {
-			assert.YAMLEq(t, expected, actual.Data[k])
-		}
-
-	})
-
-	t.Run("should return expected escaped collector config map with http_sd_config if rewrite flag disabled", func(t *testing.T) {
-		err := colfeaturegate.GlobalRegistry().Set(featuregate.EnableTargetAllocatorRewrite.ID(), false)
-		assert.NoError(t, err)
-		t.Cleanup(func() {
-			_ = colfeaturegate.GlobalRegistry().Set(featuregate.EnableTargetAllocatorRewrite.ID(), true)
-		})
-
-		expectedLables["app.kubernetes.io/component"] = "opentelemetry-collector"
-		expectedLables["app.kubernetes.io/name"] = "test-collector"
-		expectedLables["app.kubernetes.io/version"] = "latest"
-
-		expectedData := map[string]string{
-			"collector.yaml": `exporters:
-  debug:
-receivers:
-  prometheus:
-    config:
-      scrape_configs:
-      - http_sd_configs:
-        - url: http://test-targetallocator:80/jobs/serviceMonitor%2Ftest%2Ftest%2F0/targets?collector_id=$POD_NAME
-        job_name: serviceMonitor/test/test/0
-    target_allocator:
-      collector_id: ${POD_NAME}
-      endpoint: http://test-targetallocator:80
-      http_sd_config:
-        refresh_interval: 60s
-      interval: 30s
-service:
-  pipelines:
-    metrics:
-      exporters:
-      - debug
-      processors: []
-      receivers:
-      - prometheus
-`,
-		}
-
-		param, err := newParams("test/test-img", "testdata/http_sd_config_servicemonitor_test_ta_set.yaml")
-		assert.NoError(t, err)
-		param.OtelCol.Spec.TargetAllocator.Enabled = true
-		actual, err := ConfigMap(param)
-
-		assert.NoError(t, err)
-		assert.Equal(t, "test-collector", actual.Name)
-		assert.Equal(t, expectedLables, actual.Labels)
-		assert.Equal(t, len(expectedData), len(actual.Data))
-		for k, expected := range expectedData {
-			assert.YAMLEq(t, expected, actual.Data[k])
-		}
-
-		// Reset the value
-		expectedLables["app.kubernetes.io/version"] = "0.47.0"
-
 	})
 
 	t.Run("should return expected escaped collector config map with target_allocator config block", func(t *testing.T) {
