ab9aaaed25da4bf9add7c26ad15282ecbeac81c8
Jacob Aronoff <jacobaronoff45@gmail.com>
2024-04-24 13:49:46 -0400

    introduce more typing for pipelines

--- apis/v1beta1/opentelemetrycollector_types.go
+++ apis/v1beta1/opentelemetrycollector_types.go
@@ -49,8 +49,6 @@ type OpenTelemetryCollector struct {
 	Status OpenTelemetryCollectorStatus `json:"status,omitempty"`
 }
 
-func (*OpenTelemetryCollector) Hub() {}
-
 //+kubebuilder:object:root=true
 
 // OpenTelemetryCollectorList contains a list of OpenTelemetryCollector.
