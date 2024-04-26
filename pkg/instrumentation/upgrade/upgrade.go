d0fe50de7d06794f8972d7ecc84f7376147e3e2e
Yuri Sa <yurimsa@gmail.com>
2024-04-26 12:39:25 +0200

    Fixed Linters
    Signed-off-by: Yuri Sa <yurimsa@gmail.com>

--- pkg/instrumentation/upgrade/upgrade.go
+++ pkg/instrumentation/upgrade/upgrade.go
@@ -32,7 +32,7 @@ import (
 
 var (
 	defaultAnnotationToGate = map[string]*featuregate2.Gate{
-		constants.AnnotationDefaultAutoInstrumentationGo:   featuregate.EnableGoAutoInstrumentationSupport,
+		constants.AnnotationDefaultAutoInstrumentationGo: featuregate.EnableGoAutoInstrumentationSupport,
 	}
 )
 
