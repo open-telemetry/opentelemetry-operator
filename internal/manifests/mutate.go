64d8b97f1f58b7a7c397ae49033a97c4fabdc8c0
Janario Oliveira <janario.oliveira@gmail.com>
2024-05-09 13:38:36 +0200

    Change to use merge WithOverwriteWithEmptyValue
    Signed-off-by: Janario Oliveira <janario.oliveira@gmail.com>

--- internal/manifests/mutate.go
+++ internal/manifests/mutate.go
@@ -178,6 +178,10 @@ func mergeWithOverride(dst, src interface{}) error {
 	return mergo.Merge(dst, src, mergo.WithOverride)
 }
 
+func mergeWithOverwriteWithEmptyValue(dst, src interface{}) error {
+	return mergo.Merge(dst, src, mergo.WithOverwriteWithEmptyValue)
+}
+
 func mutateSecret(existing, desired *corev1.Secret) {
 	existing.Labels = desired.Labels
 	existing.Annotations = desired.Annotations
@@ -271,7 +275,7 @@ func mutateDaemonset(existing, desired *appsv1.DaemonSet) error {
 		existing.Spec.Selector = desired.Spec.Selector
 	}
 
-	if err := mergeWithOverride(&existing.Spec, desired.Spec); err != nil {
+	if err := mergeWithOverwriteWithEmptyValue(&existing.Spec, desired.Spec); err != nil {
 		return err
 	}
 	return nil
@@ -287,7 +291,7 @@ func mutateDeployment(existing, desired *appsv1.Deployment) error {
 		existing.Spec.Selector = desired.Spec.Selector
 	}
 	existing.Spec.Replicas = desired.Spec.Replicas
-	if err := mergeWithOverride(&existing.Spec.Template, desired.Spec.Template); err != nil {
+	if err := mergeWithOverwriteWithEmptyValue(&existing.Spec.Template, desired.Spec.Template); err != nil {
 		return err
 	}
 	if err := mergeWithOverride(&existing.Spec.Strategy, desired.Spec.Strategy); err != nil {
@@ -313,7 +317,7 @@ func mutateStatefulSet(existing, desired *appsv1.StatefulSet) error {
 		existing.Spec.VolumeClaimTemplates[i].ObjectMeta = desired.Spec.VolumeClaimTemplates[i].ObjectMeta
 		existing.Spec.VolumeClaimTemplates[i].Spec = desired.Spec.VolumeClaimTemplates[i].Spec
 	}
-	if err := mergeWithOverride(&existing.Spec.Template, desired.Spec.Template); err != nil {
+	if err := mergeWithOverwriteWithEmptyValue(&existing.Spec.Template, desired.Spec.Template); err != nil {
 		return err
 	}
 	return nil
