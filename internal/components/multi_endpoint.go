4c07a3b5dc8084dbec12a67ee00733d608b68c5d
Jacob Aronoff <jacobaronoff45@gmail.com>
2024-05-20 15:08:31 -0400

    Simplified parsers for 95% of use cases

---
+++ internal/components/multi_endpoint.go
@@ -0,0 +1,148 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package components
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/open-telemetry/opentelemetry-operator/internal/naming"
+)
+
+var (
+	_                  ComponentPortParser = &MultiPortReceiver{}
+	multiPortReceivers                     = []ComponentPortParser{
+		NewMultiPortReceiver("otlp",
+			WithPortMapping(
+				"grpc",
+				4317,
+				WithAppProtocol(&grpc),
+				WithTargetPort(4317),
+			), WithPortMapping(
+				"http",
+				4318,
+				WithAppProtocol(&http),
+				WithTargetPort(4318),
+			),
+		),
+		NewMultiPortReceiver("skywalking",
+			WithPortMapping(grpc, 11800,
+				WithTargetPort(11800),
+				WithAppProtocol(&grpc),
+			),
+			WithPortMapping(http, 12800,
+				WithTargetPort(12800),
+				WithAppProtocol(&http),
+			)),
+		NewMultiPortReceiver("jaeger",
+			WithPortMapping(grpc, 14250,
+				WithProtocol(corev1.ProtocolTCP),
+				WithAppProtocol(&grpc),
+			),
+			WithPortMapping("thrift_http", 14268,
+				WithProtocol(corev1.ProtocolTCP),
+				WithAppProtocol(&http),
+			),
+			WithPortMapping("thrift_compact", 6831,
+				WithProtocol(corev1.ProtocolUDP),
+			),
+			WithPortMapping("thrift_binary", 6832,
+				WithProtocol(corev1.ProtocolUDP),
+			),
+		),
+		NewMultiPortReceiver("loki",
+			WithPortMapping(grpc, 9095,
+				WithTargetPort(9095),
+				WithAppProtocol(&grpc),
+			),
+			WithPortMapping(http, 3100,
+				WithTargetPort(3100),
+				WithAppProtocol(&http),
+			),
+		),
+	}
+)
+
+// MultiProtocolEndpointConfig represents the minimal struct for a given YAML configuration input containing a map to
+// a struct with either endpoint or listen_address.
+type MultiProtocolEndpointConfig struct {
+	Protocols map[string]*SingleEndpointConfig `json:"protocols"`
+}
+
+// MultiPortOption allows the setting of options for
+type MultiPortOption func(parser *MultiPortReceiver)
+
+// MultiPortReceiver is a special parser for components with endpoints for each protocol.
+type MultiPortReceiver struct {
+	name string
+
+	portMappings map[string]*corev1.ServicePort
+}
+
+func (m *MultiPortReceiver) Ports(logger logr.Logger, config interface{}) ([]corev1.ServicePort, error) {
+	multiProtoEndpointCfg := &MultiProtocolEndpointConfig{}
+	if err := LoadMap[*MultiProtocolEndpointConfig](config, multiProtoEndpointCfg); err != nil {
+		return nil, err
+	}
+	var ports []corev1.ServicePort
+	for protocol, ec := range multiProtoEndpointCfg.Protocols {
+		if defaultSvc, ok := m.portMappings[protocol]; ok {
+			port := defaultSvc.Port
+			if ec != nil {
+				port = ec.GetPortNumOrDefault(logger, port)
+				defaultSvc.Name = naming.PortName(fmt.Sprintf("%s-%s", m.name, protocol), port)
+			}
+			ports = append(ports, ConstructServicePort(defaultSvc, port))
+		} else {
+			return nil, errors.New(fmt.Sprintf("unknown protocol set: %s", protocol))
+		}
+	}
+	return ports, nil
+}
+
+func (m *MultiPortReceiver) ParserType() string {
+	return ComponentType(m.name)
+}
+
+func (m *MultiPortReceiver) ParserName() string {
+	return fmt.Sprintf("__%s", m.name)
+}
+
+func NewMultiPortReceiver(name string, opts ...MultiPortOption) *MultiPortReceiver {
+	multiReceiver := &MultiPortReceiver{
+		name:         name,
+		portMappings: map[string]*corev1.ServicePort{},
+	}
+	for _, opt := range opts {
+		opt(multiReceiver)
+	}
+	return multiReceiver
+}
+
+func WithPortMapping(name string, port int32, opts ...PortBuilderOption) MultiPortOption {
+	return func(parser *MultiPortReceiver) {
+		servicePort := &corev1.ServicePort{
+			Name: naming.PortName(fmt.Sprintf("%s-%s", parser.name, name), port),
+			Port: port,
+		}
+		for _, opt := range opts {
+			opt(servicePort)
+		}
+		parser.portMappings[name] = servicePort
+	}
+}
---
+++ internal/components/single_endpoint_test.go
@@ -0,0 +1,122 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package components_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/open-telemetry/opentelemetry-operator/internal/components"
+	"github.com/open-telemetry/opentelemetry-operator/internal/naming"
+)
+
+var logger = logf.Log.WithName("unit-tests")
+
+func TestParseEndpoint(t *testing.T) {
+	// prepare
+	// there's no parser registered to handle "myreceiver", so, it falls back to the generic parser
+	parser := components.BuilderFor("myreceiver")
+
+	// test
+	ports, err := parser.Ports(logger, map[string]interface{}{
+		"endpoint": "0.0.0.0:1234",
+	})
+
+	// verify
+	assert.NoError(t, err)
+	assert.Len(t, ports, 1)
+	assert.EqualValues(t, 1234, ports[0].Port)
+}
+
+func TestFailedToParseEndpoint(t *testing.T) {
+	// prepare
+	// there's no parser registered to handle "myreceiver", so, it falls back to the generic parser
+	parser := components.BuilderFor("myreceiver")
+
+	// test
+	ports, err := parser.Ports(logger, map[string]interface{}{
+		"endpoint": "0.0.0.0",
+	})
+
+	// verify
+	assert.Error(t, err)
+	assert.Len(t, ports, 0)
+}
+
+func TestDownstreamParsers(t *testing.T) {
+	for _, tt := range []struct {
+		desc         string
+		receiverName string
+		parserName   string
+		defaultPort  int
+	}{
+		{"zipkin", "zipkin", "__zipkin", 9411},
+		{"opencensus", "opencensus", "__opencensus", 55678},
+
+		// contrib receivers
+		{"carbon", "carbon", "__carbon", 2003},
+		{"collectd", "collectd", "__collectd", 8081},
+		{"sapm", "sapm", "__sapm", 7276},
+		{"signalfx", "signalfx", "__signalfx", 9943},
+		{"wavefront", "wavefront", "__wavefront", 2003},
+		{"fluentforward", "fluentforward", "__fluentforward", 8006},
+		{"statsd", "statsd", "__statsd", 8125},
+		{"influxdb", "influxdb", "__influxdb", 8086},
+		{"splunk_hec", "splunk_hec", "__splunk_hec", 8088},
+		{"awsxray", "awsxray", "__awsxray", 2000},
+	} {
+		t.Run(tt.receiverName, func(t *testing.T) {
+			t.Run("builds successfully", func(t *testing.T) {
+				// test
+				parser := components.BuilderFor(tt.receiverName)
+
+				// verify
+				assert.Equal(t, tt.parserName, parser.ParserName())
+			})
+
+			t.Run("assigns the expected port", func(t *testing.T) {
+				// prepare
+				parser := components.BuilderFor(tt.receiverName)
+
+				// test
+				ports, err := parser.Ports(logger, map[string]interface{}{})
+
+				// verify
+				assert.NoError(t, err)
+				assert.Len(t, ports, 1)
+				assert.EqualValues(t, tt.defaultPort, ports[0].Port)
+				assert.Equal(t, naming.PortName(tt.receiverName, int32(tt.defaultPort)), ports[0].Name)
+			})
+
+			t.Run("allows port to be overridden", func(t *testing.T) {
+				// prepare
+				parser := components.BuilderFor(tt.receiverName)
+
+				// test
+				ports, err := parser.Ports(logger, map[string]interface{}{
+					"endpoint": "0.0.0.0:65535",
+				})
+
+				// verify
+				assert.NoError(t, err)
+				assert.Len(t, ports, 1)
+				assert.EqualValues(t, 65535, ports[0].Port)
+				assert.Equal(t, naming.PortName(tt.receiverName, int32(tt.defaultPort)), ports[0].Name)
+			})
+		})
+	}
+}
