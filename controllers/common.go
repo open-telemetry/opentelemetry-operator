195406fe145dfc572c01ef20956982d5b018044c
Jacob Aronoff <jaronoff97@users.noreply.github.com>
2024-05-21 04:12:05 -0700

    simplify deletion logic (#2971)

--- controllers/common.go
+++ controllers/common.go
@@ -22,11 +22,13 @@ import (
 	"github.com/go-logr/logr"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	"github.com/open-telemetry/opentelemetry-operator/internal/manifests"
@@ -75,6 +77,25 @@ func BuildOpAMPBridge(params manifests.Params) ([]client.Object, error) {
 		resources = append(resources, objs...)
 	}
 	return resources, nil
+}
+
+// getList queries the Kubernetes API to list the requested resource, setting the list l of type T.
+func getList[T client.Object](ctx context.Context, cl client.Client, l T, options ...client.ListOption) (map[types.UID]client.Object, error) {
+	ownedObjects := map[types.UID]client.Object{}
+	list := &unstructured.UnstructuredList{}
+	gvk, err := apiutil.GVKForObject(l, cl.Scheme())
+	if err != nil {
+		return nil, err
+	}
+	list.SetGroupVersionKind(gvk)
+	err = cl.List(ctx, list, options...)
+	if err != nil {
+		return ownedObjects, fmt.Errorf("error listing %T: %w", l, err)
+	}
+	for i := range list.Items {
+		ownedObjects[list.Items[i].GetUID()] = &list.Items[i]
+	}
+	return ownedObjects, nil
 }
 
 // reconcileDesiredObjects runs the reconcile process using the mutateFn over the given list of objects.
