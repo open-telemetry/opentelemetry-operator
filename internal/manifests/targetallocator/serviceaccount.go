9aaa6c9e366221cb9d85a758768d13f362436dca
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2024-05-22 15:12:53 +0000

    [chore] Move TargetAllocator CRD to v1alpha1 (#2918)

--- internal/manifests/targetallocator/serviceaccount.go
+++ internal/manifests/targetallocator/serviceaccount.go
@@ -18,14 +18,14 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/open-telemetry/opentelemetry-operator/apis/v1beta1"
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	"github.com/open-telemetry/opentelemetry-operator/internal/manifests"
 	"github.com/open-telemetry/opentelemetry-operator/internal/manifests/manifestutils"
 	"github.com/open-telemetry/opentelemetry-operator/internal/naming"
 )
 
 // ServiceAccountName returns the name of the existing or self-provisioned service account to use for the given instance.
-func ServiceAccountName(instance v1beta1.TargetAllocator) string {
+func ServiceAccountName(instance v1alpha1.TargetAllocator) string {
 	if len(instance.Spec.ServiceAccount) == 0 {
 		return naming.TargetAllocatorServiceAccount(instance.Name)
 	}
