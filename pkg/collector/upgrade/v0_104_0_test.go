a9251b3d65a365994c580e16dc483092fb6da428
Ben B. <bongartz@klimlive.de>
2024-07-18 13:01:54 +0200

    Prepare release 0.104.0 (#3119)
    * Prepare release 0.104.0
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * adapt to breaking changes
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * update changelog
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * otlpreceiver: bind to 0.0.0.0 by default
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    fix: config structure
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * rename
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * disable unifyEnvVarExpansion if prometheus rec is configured
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * disable component.UseLocalHostAsDefaultHost by default
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    ---------
    
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

---
+++ pkg/collector/upgrade/v0_104_0_test.go
@@ -0,0 +1,61 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package upgrade_test
+
+import (
+	"context"
+	_ "embed"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/record"
+
+	"github.com/open-telemetry/opentelemetry-operator/apis/v1beta1"
+	"github.com/open-telemetry/opentelemetry-operator/internal/version"
+	"github.com/open-telemetry/opentelemetry-operator/pkg/collector/upgrade"
+)
+
+func Test0_104_0Upgrade(t *testing.T) {
+	collectorInstance := v1beta1.OpenTelemetryCollector{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "OpenTelemetryCollector",
+			APIVersion: "v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "otel-my-instance",
+			Namespace: "somewhere",
+		},
+		Status: v1beta1.OpenTelemetryCollectorStatus{
+			Version: "0.103.0",
+		},
+		Spec: v1beta1.OpenTelemetryCollectorSpec{},
+	}
+
+	versionUpgrade := &upgrade.VersionUpgrade{
+		Log:      logger,
+		Version:  version.Get(),
+		Client:   k8sClient,
+		Recorder: record.NewFakeRecorder(upgrade.RecordBufferSize),
+	}
+
+	col, err := versionUpgrade.ManagedInstance(context.Background(), collectorInstance)
+	if err != nil {
+		t.Errorf("expect err: nil but got: %v", err)
+	}
+	assert.EqualValues(t,
+		map[string]string{"feature-gates": "-component.UseLocalHostAsDefaultHost"},
+		col.Spec.Args, "missing featuregate")
+}
