9911706c8781a05ff6b1e1ae6353f0fb69cc94bc
Haja <david.haja@gmail.com>
2024-09-29 22:23:47 +0200

    Unit tests added for additional args mutation of collector container

--- internal/manifests/mutate_test.go
+++ internal/manifests/mutate_test.go
@@ -1069,3 +1069,405 @@ func TestMutateStatefulSetAffinity(t *testing.T) {
 		})
 	}
 }
+
+func TestMutateDaemonsetCollectorArgs(t *testing.T) {
+	tests := []struct {
+		name     string
+		existing appsv1.DaemonSet
+		desired  appsv1.DaemonSet
+	}{
+		{
+			name: "add argument to collector container in daemonset",
+			existing: appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "daemonset",
+				},
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "daemonset",
+				},
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "remove extra arg from collector container in daemonset",
+			existing: appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "daemonset",
+				},
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "daemonset",
+				},
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "modify extra arg in collector container in daemonset",
+			existing: appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "daemonset",
+				},
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "daemonset",
+				},
+				Spec: appsv1.DaemonSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=no"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mutateFn := MutateFuncFor(&tt.existing, &tt.desired)
+			err := mutateFn()
+			require.NoError(t, err)
+			assert.Equal(t, tt.existing, tt.desired)
+		})
+	}
+}
+
+func TestMutateDeploymentCollectorArgs(t *testing.T) {
+	tests := []struct {
+		name     string
+		existing appsv1.Deployment
+		desired  appsv1.Deployment
+	}{
+		{
+			name: "add argument to collector container in deployment",
+			existing: appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "deployment",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "deployment",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "remove extra arg from collector container in deployment",
+			existing: appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "deployment",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "deployment",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "modify extra arg in collector container in deployment",
+			existing: appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "deployment",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "deployment",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=no"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mutateFn := MutateFuncFor(&tt.existing, &tt.desired)
+			err := mutateFn()
+			require.NoError(t, err)
+			assert.Equal(t, tt.existing, tt.desired)
+		})
+	}
+}
+
+func TestMutateStatefulSetCollectorArgs(t *testing.T) {
+	tests := []struct {
+		name     string
+		existing appsv1.StatefulSet
+		desired  appsv1.StatefulSet
+	}{
+		{
+			name: "add argument to collector container in statefulset",
+			existing: appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "statefulset",
+				},
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "statefulset",
+				},
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "remove extra arg from collector container in statefulset",
+			existing: appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "statefulset",
+				},
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "statefulset",
+				},
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "modify extra arg in collector container in statefulset",
+			existing: appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "statefulset",
+				},
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=yes"},
+								},
+							},
+						},
+					},
+				},
+			},
+			desired: appsv1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "statefulset",
+				},
+				Spec: appsv1.StatefulSetSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []corev1.Container{
+								{
+									Name:  "collector",
+									Image: "collector:latest",
+									Args:  []string{"--deafult-arg=true", "extra-arg=no"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mutateFn := MutateFuncFor(&tt.existing, &tt.desired)
+			err := mutateFn()
+			require.NoError(t, err)
+			assert.Equal(t, tt.existing, tt.desired)
+		})
+	}
+}
