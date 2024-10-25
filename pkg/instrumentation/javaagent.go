2b36f0d6f9498e3c82185a4a18f0c855c4da4a57
Pavol Loffay <p.loffay@gmail.com>
2024-10-25 19:07:23 +0200

    Support configuring java runtime from configmap or secret (env.valueFrom) (#3379)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- pkg/instrumentation/javaagent.go
+++ pkg/instrumentation/javaagent.go
@@ -24,22 +24,16 @@ import (
 
 const (
 	envJavaToolsOptions   = "JAVA_TOOL_OPTIONS"
-	javaAgent             = " -javaagent:/otel-auto-instrumentation-java/javaagent.jar"
+	javaAgent             = "-javaagent:/otel-auto-instrumentation-java/javaagent.jar"
 	javaInitContainerName = initContainerName + "-java"
 	javaVolumeName        = volumeName + "-java"
 	javaInstrMountPath    = "/otel-auto-instrumentation-java"
 )
 
-func injectJavaagent(javaSpec v1alpha1.Java, pod corev1.Pod, index int) (corev1.Pod, error) {
+func injectJavaagent(javaSpec v1alpha1.Java, pod corev1.Pod, index int) corev1.Pod {
 	volume := instrVolume(javaSpec.VolumeClaimTemplate, javaVolumeName, javaSpec.VolumeSizeLimit)
-
 	// caller checks if there is at least one container.
 	container := &pod.Spec.Containers[index]
-
-	err := validateContainerEnv(container.Env, envJavaToolsOptions)
-	if err != nil {
-		return pod, err
-	}
 
 	// inject Java instrumentation spec env vars.
 	for _, env := range javaSpec.Env {
@@ -55,14 +49,14 @@ func injectJavaagent(javaSpec v1alpha1.Java, pod corev1.Pod, index int) (corev1.
 	}
 
 	idx := getIndexOfEnv(container.Env, envJavaToolsOptions)
-	if idx == -1 {
-		container.Env = append(container.Env, corev1.EnvVar{
-			Name:  envJavaToolsOptions,
-			Value: javaJVMArgument,
-		})
-	} else {
-		container.Env[idx].Value = container.Env[idx].Value + javaJVMArgument
+	if idx != -1 {
+		// https://kubernetes.io/docs/tasks/inject-data-application/define-interdependent-environment-variables/
+		javaJVMArgument = fmt.Sprintf("$(%s) %s", envJavaToolsOptions, javaJVMArgument)
 	}
+	container.Env = append(container.Env, corev1.EnvVar{
+		Name:  envJavaToolsOptions,
+		Value: javaJVMArgument,
+	})
 
 	container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
 		Name:      volume.Name,
@@ -97,5 +91,5 @@ func injectJavaagent(javaSpec v1alpha1.Java, pod corev1.Pod, index int) (corev1.
 		}
 
 	}
-	return pod, err
+	return pod
 }
