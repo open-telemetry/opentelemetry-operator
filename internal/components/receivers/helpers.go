6bf7d732d2e9d7403a361df7227b07946cd9b22c
Israel Blancas <iblancas@redhat.com>
2024-11-11 09:41:07 +0100

    Add automatic RBAC creation for k8sobjects receiver
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- internal/components/receivers/helpers.go
+++ internal/components/receivers/helpers.go
@@ -143,6 +143,9 @@ var (
 		components.NewBuilder[k8seventsConfig]().WithName("k8s_events").
 			WithRbacGen(generatek8seventsRbacRules).
 			MustBuild(),
+		components.NewBuilder[k8sobjectsConfig]().WithName("k8sobjects").
+			WithRbacGen(generatek8sobjectsRbacRules).
+			MustBuild(),
 		NewScraperParser("prometheus"),
 		NewScraperParser("sshcheck"),
 		NewScraperParser("cloudfoundry"),
