5e35dbb4a8c4cae8bb6e0cbad130e7d2ee0d3c2e
Israel Blancas <iblancas@redhat.com>
2024-11-23 16:46:53 +0100

    Add automatic RBAC creation for k8scluster receiver (#3428)
    Signed-off-by: Israel Blancas <iblancas@redhat.com>

--- internal/components/receivers/helpers.go
+++ internal/components/receivers/helpers.go
@@ -143,6 +143,9 @@ var (
 		components.NewBuilder[k8seventsConfig]().WithName("k8s_events").
 			WithRbacGen(generatek8seventsRbacRules).
 			MustBuild(),
+		components.NewBuilder[k8sclusterConfig]().WithName("k8s_cluster").
+			WithRbacGen(generatek8sclusterRbacRules).
+			MustBuild(),
 		components.NewBuilder[k8sobjectsConfig]().WithName("k8sobjects").
 			WithRbacGen(generatek8sobjectsRbacRules).
 			MustBuild(),
