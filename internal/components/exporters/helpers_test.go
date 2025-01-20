162076647b58782a97880f25ef4fb4fb8c189334
Antoine Toulme <atoulme@splunk.com>
2025-01-19 21:20:26 -0800

    [chore] simplify exporter code

--- internal/components/exporters/helpers_test.go
+++ internal/components/exporters/helpers_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package exporters_test
+package exporters
 
 import (
 	"testing"
@@ -21,13 +21,12 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/open-telemetry/opentelemetry-operator/internal/components"
-	"github.com/open-telemetry/opentelemetry-operator/internal/components/exporters"
 	"github.com/open-telemetry/opentelemetry-operator/internal/naming"
 )
 
 func TestParserForReturns(t *testing.T) {
 	const testComponentName = "test"
-	parser := exporters.ParserFor(testComponentName)
+	parser := ParserFor(testComponentName)
 	assert.Equal(t, "test", parser.ParserType())
 	assert.Equal(t, "__test", parser.ParserName())
 	ports, err := parser.Ports(logr.Discard(), testComponentName, map[string]interface{}{
@@ -39,9 +38,8 @@ func TestParserForReturns(t *testing.T) {
 
 func TestCanRegister(t *testing.T) {
 	const testComponentName = "test"
-	exporters.Register(testComponentName, components.NewSinglePortParserBuilder(testComponentName, 9000).MustBuild())
-	assert.True(t, exporters.IsRegistered(testComponentName))
-	parser := exporters.ParserFor(testComponentName)
+	registry[testComponentName] = components.NewSinglePortParserBuilder(testComponentName, 9000).MustBuild()
+	parser := ParserFor(testComponentName)
 	assert.Equal(t, "test", parser.ParserType())
 	assert.Equal(t, "__test", parser.ParserName())
 	ports, err := parser.Ports(logr.Discard(), testComponentName, map[string]interface{}{})
@@ -60,11 +58,12 @@ func TestExporterComponentParsers(t *testing.T) {
 	} {
 		t.Run(tt.exporterName, func(t *testing.T) {
 			t.Run("is registered", func(t *testing.T) {
-				assert.True(t, exporters.IsRegistered(tt.exporterName))
+				_, ok := registry[tt.exporterName]
+				assert.True(t, ok)
 			})
 			t.Run("bad config errors", func(t *testing.T) {
 				// prepare
-				parser := exporters.ParserFor(tt.exporterName)
+				parser := ParserFor(tt.exporterName)
 
 				// test throwing in pure junk
 				_, err := parser.Ports(logr.Discard(), tt.exporterName, func() {})
@@ -75,7 +74,7 @@ func TestExporterComponentParsers(t *testing.T) {
 
 			t.Run("assigns the expected port", func(t *testing.T) {
 				// prepare
-				parser := exporters.ParserFor(tt.exporterName)
+				parser := ParserFor(tt.exporterName)
 
 				// test
 				ports, err := parser.Ports(logr.Discard(), tt.exporterName, map[string]interface{}{})
@@ -93,7 +92,7 @@ func TestExporterComponentParsers(t *testing.T) {
 
 			t.Run("allows port to be overridden", func(t *testing.T) {
 				// prepare
-				parser := exporters.ParserFor(tt.exporterName)
+				parser := ParserFor(tt.exporterName)
 
 				// test
 				ports, err := parser.Ports(logr.Discard(), tt.exporterName, map[string]interface{}{
