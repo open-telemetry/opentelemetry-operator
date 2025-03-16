799e294ff060ffef61abeafec94e2b3213a8466f
Charlie Le <charlie_le@apple.com>
2025-03-16 13:32:33 -0700

    Add UI for scrape_configs, embed html templates, and setup golden files
    Signed-off-by: Charlie Le <charlie_le@apple.com>

---
+++ cmd/otel-allocator/internal/server/templates.go
@@ -0,0 +1,69 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package server
+
+import (
+	_ "embed"
+	"html/template"
+	"io"
+	"log"
+)
+
+var (
+	templateFunctions = template.FuncMap{
+		"even": even,
+	}
+
+	//go:embed templates/page_header.html
+	headerBytes    []byte
+	headerTemplate = parseTemplate("header", headerBytes)
+
+	//go:embed templates/page_footer.html
+	footerBytes    []byte
+	footerTemplate = parseTemplate("footer", footerBytes)
+
+	//go:embed templates/properties_table.html
+	propertiesTableBytes    []byte
+	propertiesTableTemplate = parseTemplate("properties_table", propertiesTableBytes)
+)
+
+func parseTemplate(name string, bytes []byte) *template.Template {
+	return template.Must(template.New(name).Funcs(templateFunctions).Parse(string(bytes)))
+}
+
+// HeaderData contains data for the header template.
+type HeaderData struct {
+	Title string
+}
+
+// WriteHTMLPageHeader writes the header.
+func WriteHTMLPageHeader(w io.Writer, hd HeaderData) {
+	if err := headerTemplate.Execute(w, hd); err != nil {
+		log.Printf("ta: executing template: %v", err)
+	}
+}
+
+// PropertiesTableData contains data for properties table template.
+type PropertiesTableData struct {
+	Headers []string
+	Rows    [][]template.HTML
+}
+
+// WriteHTMLPropertiesTable writes the HTML for properties table.
+func WriteHTMLPropertiesTable(w io.Writer, chd PropertiesTableData) {
+	if err := propertiesTableTemplate.Execute(w, chd); err != nil {
+		log.Printf("ta: executing template: %v", err)
+	}
+}
+
+// WriteHTMLPageFooter writes the footer.
+func WriteHTMLPageFooter(w io.Writer) {
+	if err := footerTemplate.Execute(w, nil); err != nil {
+		log.Printf("ta: executing template: %v", err)
+	}
+}
+
+func even(x int) bool {
+	return x%2 == 0
+}
