15e013f61e4eb69625fbbee70e3636cec599e73f
Charlie Le <charlie_le@apple.com>
2025-07-08 05:48:44 -0700

    ta: inline tls config for config generation (#4146)
    * ta: inline tls config for config generation
    
    fixes: #3724
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * add changelog
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * retry getting ta metrics
    
    ta could come up after the collector which could cause it to go into a
    clbo. this race condition can make the metrics endpoint unavailable and
    cause the test to be flaky.
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * improve testing conditions for less flakiness
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * use x_metrics_decode for parsing metric value
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * Update chainsaw-test.yaml
    
    ---------
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>

--- cmd/otel-allocator/internal/watcher/promOperator.go
+++ cmd/otel-allocator/internal/watcher/promOperator.go
@@ -86,7 +86,7 @@ func NewPrometheusCRWatcher(
 		},
 	}
 
-	generator, err := prometheus.NewConfigGenerator(promLogger, prom, prometheus.WithEndpointSliceSupport())
+	generator, err := prometheus.NewConfigGenerator(promLogger, prom, prometheus.WithEndpointSliceSupport(), prometheus.WithInlineTLSConfig())
 
 	if err != nil {
 		return nil, err
