4ab7a05e1893233513fb901962663d2ed8775810
Praful Khanduri <99384392+Horiodino@users.noreply.github.com>
2025-07-16 18:45:46 +0530

    [chore] Add extra labels to  spec observability metrics (#4174)
    * Add extraLabels support to spec.observability.metrics
    
    Signed-off-by: Praful Khanduri <holiodin@gmail.com>
    
    * docs: add support for extraLabels in spec.observability.metrics
    
    Signed-off-by: Praful Khanduri <holiodin@gmail.com>
    
    * Add e2e test for extra labels
    
    Signed-off-by: Praful Khanduri <holiodin@gmail.com>
    
    * Add extra labels for target allocator
    
    Signed-off-by: Praful Khanduri <holiodin@gmail.com>
    
    ---------
    
    Signed-off-by: Praful Khanduri <holiodin@gmail.com>

--- internal/manifests/collector/servicemonitor_test.go
+++ internal/manifests/collector/servicemonitor_test.go
@@ -78,3 +78,82 @@ func TestDesiredServiceMonitorsPrometheusNotAvailable(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Nil(t, actual)
 }
+
+func TestDesiredServiceMonitorsWithEmptyExtraLabels(t *testing.T) {
+	params, err := newParams("", "testdata/prometheus-exporter.yaml", nil)
+	assert.NoError(t, err)
+	params.OtelCol.Spec.Observability.Metrics.EnableMetrics = true
+
+	params.OtelCol.Spec.Observability.Metrics.ExtraLabels = nil
+
+	actual, err := ServiceMonitor(params)
+	assert.NoError(t, err)
+	assert.NotNil(t, actual)
+	assert.Equal(t, fmt.Sprintf("%s-collector", params.OtelCol.Name), actual.Name)
+	assert.Equal(t, params.OtelCol.Namespace, actual.Namespace)
+	assert.Equal(t, "prometheus-dev", actual.Spec.Endpoints[0].Port)
+	assert.Equal(t, "prometheus-prod", actual.Spec.Endpoints[1].Port)
+
+	expectedLabels := map[string]string{
+		"app.kubernetes.io/component":  "opentelemetry-collector",
+		"app.kubernetes.io/instance":   "default.test",
+		"app.kubernetes.io/managed-by": "opentelemetry-operator",
+		"app.kubernetes.io/part-of":    "opentelemetry",
+		"app.kubernetes.io/name":       "test-collector",
+		"app.kubernetes.io/version":    "latest",
+	}
+	assert.Equal(t, expectedLabels, actual.ObjectMeta.Labels)
+
+	expectedSelectorLabels := map[string]string{
+		"app.kubernetes.io/component":                      "opentelemetry-collector",
+		"app.kubernetes.io/instance":                       "default.test",
+		"app.kubernetes.io/managed-by":                     "opentelemetry-operator",
+		"app.kubernetes.io/part-of":                        "opentelemetry",
+		"operator.opentelemetry.io/collector-service-type": "base",
+	}
+	assert.Equal(t, expectedSelectorLabels, actual.Spec.Selector.MatchLabels)
+}
+
+func TestDesiredServiceMonitorsWithExtraLabels(t *testing.T) {
+	params, err := newParams("", "testdata/prometheus-exporter.yaml", nil)
+	assert.NoError(t, err)
+	params.OtelCol.Spec.Observability.Metrics.EnableMetrics = true
+
+	params.OtelCol.Spec.Observability.Metrics.ExtraLabels = map[string]string{
+		"prometheus":    "kube-prometheus",
+		"team":          "platform",
+		"environment":   "production",
+		"custom.io/key": "custom-value",
+	}
+
+	actual, err := ServiceMonitor(params)
+	assert.NoError(t, err)
+	assert.NotNil(t, actual)
+	assert.Equal(t, fmt.Sprintf("%s-collector", params.OtelCol.Name), actual.Name)
+	assert.Equal(t, params.OtelCol.Namespace, actual.Namespace)
+	assert.Equal(t, "prometheus-dev", actual.Spec.Endpoints[0].Port)
+	assert.Equal(t, "prometheus-prod", actual.Spec.Endpoints[1].Port)
+
+	expectedLabels := map[string]string{
+		"app.kubernetes.io/component":  "opentelemetry-collector",
+		"app.kubernetes.io/instance":   "default.test",
+		"app.kubernetes.io/managed-by": "opentelemetry-operator",
+		"app.kubernetes.io/part-of":    "opentelemetry",
+		"app.kubernetes.io/name":       "test-collector",
+		"app.kubernetes.io/version":    "latest",
+		"prometheus":                   "kube-prometheus",
+		"team":                         "platform",
+		"environment":                  "production",
+		"custom.io/key":                "custom-value",
+	}
+	assert.Equal(t, expectedLabels, actual.ObjectMeta.Labels)
+
+	expectedSelectorLabels := map[string]string{
+		"app.kubernetes.io/component":                      "opentelemetry-collector",
+		"app.kubernetes.io/instance":                       "default.test",
+		"app.kubernetes.io/managed-by":                     "opentelemetry-operator",
+		"app.kubernetes.io/part-of":                        "opentelemetry",
+		"operator.opentelemetry.io/collector-service-type": "base",
+	}
+	assert.Equal(t, expectedSelectorLabels, actual.Spec.Selector.MatchLabels)
+}
