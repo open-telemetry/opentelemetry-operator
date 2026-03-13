10e03c11a8ddc23a42f4c0eb920fd203954f3a7a
Matthieu MOREL <matthieu.morel35@gmail.com>
2026-03-13 08:28:46 +0100

    [chore]: fix commentedOutCode issues from gocritic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- cmd/otel-allocator/internal/prehook/relabel_test.go
+++ cmd/otel-allocator/internal/prehook/relabel_test.go
@@ -259,7 +259,6 @@ func TestApply(t *testing.T) {
 		relabelCfg[key] = nil
 	}
 
-	// cfg = createMockConfig(relabelCfg)
 	allocatorPrehook.SetConfig(relabelCfg)
 	remainingItems = allocatorPrehook.Apply(targets)
 	// relabelCfg is empty so targets should be unfiltered
