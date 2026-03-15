eb210a533111dc56ba4edc55e28278bb639b6e6e
Antoine Toulme <atoulme@splunk.com>
2026-03-14 20:45:04 -0700

    do not touch upgrades

--- internal/instrumentation/upgrade/upgrade_test.go
+++ internal/instrumentation/upgrade/upgrade_test.go
@@ -45,7 +45,6 @@ func TestUpgrade(t *testing.T) {
 	}
 
 	cfg := config.Config{
-		EnableInstrumentationCRDs:           true,
 		AutoInstrumentationJavaImage:        "java:1",
 		AutoInstrumentationNodeJSImage:      "nodejs:1",
 		AutoInstrumentationPythonImage:      "python:1",
@@ -78,7 +77,6 @@ func TestUpgrade(t *testing.T) {
 	require.NoError(t, err)
 
 	cfg = config.Config{
-		EnableInstrumentationCRDs:           true,
 		AutoInstrumentationJavaImage:        "java:2",
 		AutoInstrumentationNodeJSImage:      "nodejs:2",
 		AutoInstrumentationPythonImage:      "python:2",
@@ -96,7 +94,7 @@ func TestUpgrade(t *testing.T) {
 	}
 	up := NewInstrumentationUpgrade(k8sClient, ctrl.Log.WithName("instrumentation-upgrade"), &events.FakeRecorder{}, cfg)
 
-	err = up.ManagedInstances(context.Background(), cfg)
+	err = up.ManagedInstances(context.Background())
 	require.NoError(t, err)
 
 	updated := v1alpha1.Instrumentation{}
