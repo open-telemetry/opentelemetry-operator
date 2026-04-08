258f81eb2c0d17b6a601fde3a1d0548fc8c4df29
Mikołaj Świątek <mail@mikolajswiatek.com>
2026-04-08 13:55:56 +0200

    Use a list for setting copy args when injecting HTTPD instrumentation

--- internal/instrumentation/apachehttpd.go
+++ internal/instrumentation/apachehttpd.go
@@ -5,6 +5,7 @@ package instrumentation
 
 import (
 	"fmt"
+	"path/filepath"
 	"slices"
 	"strings"
 
@@ -69,11 +70,12 @@ func injectApacheHttpdagent(_ logr.Logger, apacheSpec v1alpha1.ApacheHttpd, pod 
 
 		apacheConfDir := getApacheConfDir(apacheSpec.ConfigPath)
 
+		// don't use filepath.Join here because we want to keep the dot at the end
+		apacheConfDirDestinationPath := strings.Join([]string{apacheConfDir, "."}, string(filepath.Separator))
 		cloneContainer := corev1.Container{
 			Name:    apacheAgentCloneContainerName,
 			Image:   container.Image,
-			Command: []string{"/bin/sh", "-c"},
-			Args:    []string{"cp -r " + apacheConfDir + "/* " + apacheAgentConfDirFull},
+			Command: []string{"cp", "-r", apacheConfDirDestinationPath, apacheAgentConfDirFull},
 			Env:     container.Env,
 			EnvFrom: container.EnvFrom,
 			VolumeMounts: slices.Concat(container.VolumeMounts, []corev1.VolumeMount{{
