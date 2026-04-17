5c0af91cb116c2302bb41d3b67a6bf9dc75beca9
Bejal Lewis <164711649+blewis12@users.noreply.github.com>
2026-04-17 15:59:14 +0200

    add collector args to operator

--- internal/manifests/collector/container_test.go
+++ internal/manifests/collector/container_test.go
@@ -88,6 +88,38 @@ func TestContainerWithImageOverridden(t *testing.T) {
 	assert.Equal(t, "overridden-image", c.Image)
 }
 
+func TestContainerCommandPassthrough(t *testing.T) {
+	otelcol := v1beta1.OpenTelemetryCollector{
+		Spec: v1beta1.OpenTelemetryCollectorSpec{
+			Command: &v1beta1.CollectorCommand{
+				Name:      "/usr/share/agent",
+				ExtraArgs: []string{"otel"},
+			},
+		},
+	}
+	cfg := config.Config{CollectorImage: "default-image"}
+	c := Container(cfg, testLogger, otelcol, true)
+
+	assert.Equal(t, []string{"/usr/share/agent", "otel"}, c.Command)
+	assert.Contains(t, c.Args[0], "--config=")
+}
+
+func TestContainerCommandOmittedWhenUnset(t *testing.T) {
+	otelcol := v1beta1.OpenTelemetryCollector{Spec: v1beta1.OpenTelemetryCollectorSpec{}}
+	c := Container(config.Config{CollectorImage: "default-image"}, testLogger, otelcol, true)
+	assert.Nil(t, c.Command)
+}
+
+func TestContainerCommandNameOnly(t *testing.T) {
+	otelcol := v1beta1.OpenTelemetryCollector{
+		Spec: v1beta1.OpenTelemetryCollectorSpec{
+			Command: &v1beta1.CollectorCommand{Name: "/otelcol"},
+		},
+	}
+	c := Container(config.Config{CollectorImage: "default-image"}, testLogger, otelcol, true)
+	assert.Equal(t, []string{"/otelcol"}, c.Command)
+}
+
 func TestContainerPorts(t *testing.T) {
 	goodConfig := `receivers:
   examplereceiver:
