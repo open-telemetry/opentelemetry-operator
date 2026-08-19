dc26fe815a0f29da98715bf7332a5e83b43544ae
Jerry Leung <jerrytfleung@users.noreply.github.com>
2026-08-19 16:01:45 -0700

    Merge fixes (#25)
    * Merged the fixes back

--- internal/instrumentation/scripts/php_agent.sh
+++ internal/instrumentation/scripts/php_agent.sh
@@ -20,3 +20,4 @@ api=$(cat "$clone"/api.txt)
 standard_c_lib=$(cat "$clone"/standard_c_lib.txt)
 
 cp -rf "$instrumentation_src"/"$api"/"$standard_c_lib"/"$thread_safety"/* "$mounted_dir"/
+cp -rf "$instrumentation_src"/opentelemetry.ini "$mounted_dir"/
